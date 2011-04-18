238f6f9bbf541a10f2db94208f2101b206c0b923
patriciapaula <patricia@virtual.ufc.br>
2011-04-18 11:04:26 -0300

    [#6560421] trabalhando na tela de participantes...

---
+++ app/controllers/participants_controller.rb
@@ -0,0 +1,34 @@
+class ParticipantsController < ApplicationController
+
+  include ParticipantsHelper
+
+  # load_and_authorize_resource
+  # skip_authorize_resource :only => :showoffersbyuser
+
+  before_filter :require_user, :only => [:show]
+
+  # exibe participantes da unidade curricular
+  def show
+    name = session[:active_tab]
+    id = session[:opened_tabs][name]["id"]
+
+    if id
+      #localiza unidade curricular ativa
+      @curriculum_unit = CurriculumUnit.find (id)
+
+      #retorna perfil em que se pede matricula (~aluno)
+      @student_profile = student_profile
+
+      #retorna responsaveis pela turma
+      @responsible = class_participants id, true
+      
+      #retorna participantes da turma (que nao sejam responsaveis)
+      @participants = class_participants id, false
+
+      if current_user
+        @user = User.find(current_user.id)
+      end
+    end    
+  end
+
+end
