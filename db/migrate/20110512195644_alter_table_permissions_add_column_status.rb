5470793fcb2e069d12338d8de9d7ea60b4caea9f
Wedson Lima <wedson.lima@virtual.ufc.br>
2011-05-13 09:14:26 -0300

    [#12509557] Migrates da tabela de menus

--- db/migrate/20110512195644_alter_table_permissions_add_column_status.rb
+++ db/migrate/20110512195644_alter_table_permissions_add_column_status.rb
@@ -3,20 +3,11 @@ class AlterTablePermissionsAddColumnStatus < ActiveRecord::Migration
     change_table :permissions do |t|
       t.boolean :status, :default => true
     end
-
-    execute <<-SQL
-      ALTER TABLE permissions ADD PRIMARY KEY (profiles_id, resources_id)
-    SQL
-
   end
 
   def self.down
     change_table :permissions do |t|
       t.remove :status
     end
-
-    execute <<-SQL
-      ALTER TABLE permissions DROP CONSTRAINT permissions_pkey
-    SQL
   end
 end
