5776dcfdf42d35fa993b4f9999bb4d1da159c3c3
patriciapaula <patricia@virtual.ufc.br>
2011-05-16 10:23:00 -0300

    [#6560389] implementando controle de acesso aos arquivos das aulas - so permite acessar arquivos de aula se aba da disciplina relacionada estiver aberta.

--- app/controllers/access_control_controller.rb
+++ app/controllers/access_control_controller.rb
@@ -20,38 +20,84 @@ class AccessControlController < ApplicationController
   end
 
   def lesson
-    # FALTA REGRAS
-    # verificar se usuario logado tem aula passada em alguma disciplina matriculada
-    # 
-    # ************
 
-    case params[:extension]
-    when "jpg", "jpeg"
-      type = 'image/jpeg'
-    when "gif"
-      type = 'image/gif'
-    when "swf"
-      type = 'application/x-shockwave-flash'
-    when "pdf"
-      type = 'application/pdf'
-    when "htm", "html"
-      type = 'text/html; charset=utf-8'
-    when "doc"
-      type = 'application/msword'
-    when "docx"
-      type = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-    when "ppt"
-      type = 'application/vnd.ms-powerpoint'
-    when "pptx"
-      type = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
-    when "txt"
-      type = 'text/plain'
-    else
-      type = "application/octet-stream"
+    # verificar se usuario logado tem aula passada em na(s) disciplina(s) aberta(s)
+
+    groups = ""
+    offers = ""
+
+    # pega valores de offer e group das abas abertas pra consultar aulas
+    tabs = session[:opened_tabs]
+    tabs.each do |key, value|
+      if (!value["groups_id"].nil?)
+        groups += "," unless groups==""
+        groups += value["groups_id"]
+      end
+      if (!value["offers_id"].nil?)
+        offers += "," unless offers==""
+        offers += value["offers_id"]
+      end
+    end
+
+    # *** mesma query usada no controller lessons, so MUDA q no where tem o ID da LESSON ***
+    # consulta aulas com acesso permitido
+    query_lessons = "select * from (SELECT distinct at.id as id, at.offers_id as offerid, l.id as lessonid,
+                           l.allocation_tags_id as alloctagid,
+                           l.name, description, address, l.type_lesson, privacy, l.order, l.start, l.end
+                      FROM lessons l
+                      LEFT JOIN allocation_tags at ON l.allocation_tags_id = at.id
+                    WHERE
+                      status=#{Lesson_Approved} and l.start<=current_date and l.end>=current_date
+                      and (at.offers_id in ( #{offers} )) 
+                      and l.id=#{params[:id]}              -- filtra pela aula passada
+                    ORDER BY L.order) as query_offer
+
+                    UNION ALL
+
+                    select * from (SELECT distinct at.id as id, at.offers_id as offerid, l.id as lessonid,
+                           l.allocation_tags_id as alloctagid,
+                           l.name, description, address, l.type_lesson, privacy, l.order, l.start, l.end
+                      FROM lessons l
+                      LEFT JOIN allocation_tags at ON l.allocation_tags_id = at.id
+                    WHERE
+                      status=#{Lesson_Approved} and l.start<=current_date and l.end>=current_date
+                      and (at.groups_id in ( #{groups} )) 
+                      and l.id=#{params[:id]}             -- filtra pela aula passada
+                    ORDER BY L.order) as query_group"
+    
+    permited_lessons = Lesson.find_by_sql(query_lessons)
+    
+    # se tem aula passada em disciplina aberta, pode acessar
+    if (permited_lessons.length>0)
+        case params[:extension]
+        when "jpg", "jpeg"
+          type = 'image/jpeg'
+        when "gif"
+          type = 'image/gif'
+        when "swf"
+          type = 'application/x-shockwave-flash'
+        when "pdf"
+          type = 'application/pdf'
+        when "htm", "html"
+          type = 'text/html; charset=utf-8'
+        when "doc"
+          type = 'application/msword'
+        when "docx"
+          type = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+        when "ppt"
+          type = 'application/vnd.ms-powerpoint'
+        when "pptx"
+          type = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+        when "txt"
+          type = 'text/plain'
+        else
+          type = "application/octet-stream"
+        end
+
+        # path do arquivo da aula
+        send_file("#{Rails.root}/media/lessons/#{params[:id]}/#{params[:file]}.#{params[:extension]}", { :disposition => 'inline', :type => type} )
     end
     
-    #path do arquivo da aula
-    send_file("#{Rails.root}/media/lessons/#{params[:id]}/#{params[:file]}.#{params[:extension]}", { :disposition => 'inline', :type => type} )
   end
 
 end
