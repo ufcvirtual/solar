56edec85a6a42a719c285ae126242eb41e9e4625
patriciapaula <patricia@virtual.ufc.br>
2011-05-20 10:52:30 -0300

    [#master] esqueci de alterar antes de mandar...

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,8 +22,5 @@ Solar::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
-
-  #mudando armazenamento de sessao para o banco
-  config.action_dispatch.session_store = :active_record_store  
 end
 
