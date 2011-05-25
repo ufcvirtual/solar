49a55dcad4328e3112427a378e081aca797d8b01
rcpalacio <ricardo@virtual.ufc.br>
2011-05-25 15:45:38 -0300

    [#13646383] fase inicial do forum

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,5 +1,9 @@
 class DiscussionsController < ApplicationController
   
+  include DiscussionPostsHelper
+
+  #load_and_authorize_resource #Setar permissoes!!!!!
+
   def list
     
     # pegando dados da sessao e nao da url
@@ -39,87 +43,97 @@ class DiscussionsController < ApplicationController
 
   end
   
+  def show
+    discussion_id = params[:id]
+    @discussion = Discussion.find(discussion_id)
+    @posts = return_discussion_posts(discussion_id, true)# método no helper. Quais variaveis?
+  end
+
 end
 
-  # # GET /discussions
-  # # GET /discussions.xml
-  # def index
-    # @discussions = Discussion.all
+
+
+
+
+# # GET /discussions
+# # GET /discussions.xml
+# def index
+# @discussions = Discussion.all
 # 
-    # respond_to do |format|
-      # format.html # index.html.erb
-      # format.xml  { render :xml => @discussions }
-    # end
-  # end
+# respond_to do |format|
+# format.html # index.html.erb
+# format.xml  { render :xml => @discussions }
+# end
+# end
 # 
-  # # GET /discussions/1
-  # # GET /discussions/1.xml
-  # def show
-    # @discussion = Discussion.find(params[:id])
+# # GET /discussions/1
+# # GET /discussions/1.xml
+# def show
+# @discussion = Discussion.find(params[:id])
 # 
-    # respond_to do |format|
-      # format.html # show.html.erb
-      # format.xml  { render :xml => @discussion }
-    # end
-  # end
+# respond_to do |format|
+# format.html # show.html.erb
+# format.xml  { render :xml => @discussion }
+# end
+# end
 # 
-  # # GET /discussions/new
-  # # GET /discussions/new.xml
-  # def new
-    # @discussion = Discussion.new
+# # GET /discussions/new
+# # GET /discussions/new.xml
+# def new
+# @discussion = Discussion.new
 # 
-    # respond_to do |format|
-      # format.html # new.html.erb
-      # format.xml  { render :xml => @discussion }
-    # end
-  # end
+# respond_to do |format|
+# format.html # new.html.erb
+# format.xml  { render :xml => @discussion }
+# end
+# end
 # 
-  # # GET /discussions/1/edit
-  # def edit
-    # @discussion = Discussion.find(params[:id])
-  # end
+# # GET /discussions/1/edit
+# def edit
+# @discussion = Discussion.find(params[:id])
+# end
 # 
-  # # POST /discussions
-  # # POST /discussions.xml
-  # def create
-    # @discussion = Discussion.new(params[:discussion])
+# # POST /discussions
+# # POST /discussions.xml
+# def create
+# @discussion = Discussion.new(params[:discussion])
 # 
-    # respond_to do |format|
-      # if @discussion.save
-        # format.html { redirect_to(@discussion, :notice => 'Discussion was successfully created.') }
-        # format.xml  { render :xml => @discussion, :status => :created, :location => @discussion }
-      # else
-        # format.html { render :action => "new" }
-        # format.xml  { render :xml => @discussion.errors, :status => :unprocessable_entity }
-      # end
-    # end
-  # end
+# respond_to do |format|
+# if @discussion.save
+# format.html { redirect_to(@discussion, :notice => 'Discussion was successfully created.') }
+# format.xml  { render :xml => @discussion, :status => :created, :location => @discussion }
+# else
+# format.html { render :action => "new" }
+# format.xml  { render :xml => @discussion.errors, :status => :unprocessable_entity }
+# end
+# end
+# end
 # 
-  # # PUT /discussions/1
-  # # PUT /discussions/1.xml
-  # def update
-    # @discussion = Discussion.find(params[:id])
+# # PUT /discussions/1
+# # PUT /discussions/1.xml
+# def update
+# @discussion = Discussion.find(params[:id])
 # 
-    # respond_to do |format|
-      # if @discussion.update_attributes(params[:discussion])
-        # format.html { redirect_to(@discussion, :notice => 'Discussion was successfully updated.') }
-        # format.xml  { head :ok }
-      # else
-        # format.html { render :action => "edit" }
-        # format.xml  { render :xml => @discussion.errors, :status => :unprocessable_entity }
-      # end
-    # end
-  # end
+# respond_to do |format|
+# if @discussion.update_attributes(params[:discussion])
+# format.html { redirect_to(@discussion, :notice => 'Discussion was successfully updated.') }
+# format.xml  { head :ok }
+# else
+# format.html { render :action => "edit" }
+# format.xml  { render :xml => @discussion.errors, :status => :unprocessable_entity }
+# end
+# end
+# end
 # 
-  # # DELETE /discussions/1
-  # # DELETE /discussions/1.xml
-  # def destroy
-    # @discussion = Discussion.find(params[:id])
-    # @discussion.destroy
+# # DELETE /discussions/1
+# # DELETE /discussions/1.xml
+# def destroy
+# @discussion = Discussion.find(params[:id])
+# @discussion.destroy
 # 
-    # respond_to do |format|
-      # format.html { redirect_to(discussions_url) }
-      # format.xml  { head :ok }
-    # end
-  # end
+# respond_to do |format|
+# format.html { redirect_to(discussions_url) }
+# format.xml  { head :ok }
+# end
+# end
 # end
---
+++ app/helpers/discussion_posts_helper.rb
@@ -0,0 +1,27 @@
+module DiscussionPostsHelper
+
+
+  #Renderiza um post na tela de interação do portólio.
+  #show_child indica se as respostas deste post devem ser renderizadas com ele.
+  def show_post(post_id = nil, show_child=true)
+    render '/discussions/post'
+  end
+
+
+  #Recupera os posts de uma discussion.
+  def return_discussion_posts(discussion_id = nil, show_child=true)
+    query = "select
+                id, discussions_id, users_id, content, father_id, created_at, updated_at
+             from
+                discussion_posts
+             where
+                discussions_id = '#{discussion_id}'"
+    if !(show_child)
+      query += " and father_id is null"
+    end
+    query += " order by created_at desc"
+
+    return DiscussionPost.find_by_sql(query)
+  end
+
+end
