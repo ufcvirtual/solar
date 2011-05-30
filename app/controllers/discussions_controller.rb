a58a86c630c4ccc718d8497c0d1b36b9ece45eaa
Humberto Gomes <humberto@virtual.ufc.br>
2011-05-30 18:15:09 -0300

    [#13646393] Fazendo discussion posts. Precisa ser revisto

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,15 +1,14 @@
 class DiscussionsController < ApplicationController
-  
+
   include DiscussionPostsHelper
 
   #load_and_authorize_resource #Setar permissoes!!!!!
-
   def list
-    
+
     # pegando dados da sessao e nao da url
     group_id = session[:opened_tabs][session[:active_tab]]["group_id"]
     offer_id = session[:opened_tabs][session[:active_tab]]["offer_id"]
-    
+
     if group_id.nil?
       group_id = -1
     end
@@ -42,62 +41,72 @@ class DiscussionsController < ApplicationController
     @discussions = Discussion.find_by_sql(query)
 
   end
-  
+
   def show
     discussion_id = params[:id]
     @discussion = Discussion.find(discussion_id)
     @posts = return_discussion_posts(discussion_id, true)# método no helper. Quais variaveis?
   end
 
+  def new_post
+    discussion_id = params[:discussion_id]
+    profile_id = 2
+    content = params[:new_discussion_post][:content]
+
+    new_discussion_post = DiscussionPost.new :discussion_id => discussion_id, :user_id => current_user.id, :profile_id => profile_id, :content => content
+    new_discussion_post.save
+
+    @discussion = Discussion.find(discussion_id)
+    @posts = return_discussion_posts(discussion_id, true)# método no helper. Quais variaveis?
+    # show
+    render "show"
+  end
+
 end
-
-
-
-
 
 # # GET /discussions
 # # GET /discussions.xml
 # def index
 # @discussions = Discussion.all
-# 
+#
 # respond_to do |format|
 # format.html # index.html.erb
 # format.xml  { render :xml => @discussions }
 # end
 # end
-# 
+#
 # # GET /discussions/1
 # # GET /discussions/1.xml
 # def show
 # @discussion = Discussion.find(params[:id])
-# 
+#
 # respond_to do |format|
 # format.html # show.html.erb
 # format.xml  { render :xml => @discussion }
 # end
 # end
-# 
+#
 # # GET /discussions/new
 # # GET /discussions/new.xml
 # def new
 # @discussion = Discussion.new
-# 
+#
 # respond_to do |format|
 # format.html # new.html.erb
 # format.xml  { render :xml => @discussion }
 # end
 # end
-# 
+#
 # # GET /discussions/1/edit
 # def edit
 # @discussion = Discussion.find(params[:id])
 # end
-# 
+#
 # # POST /discussions
 # # POST /discussions.xml
 # def create
 # @discussion = Discussion.new(params[:discussion])
-# 
+#
 # respond_to do |format|
 # if @discussion.save
 # format.html { redirect_to(@discussion, :notice => 'Discussion was successfully created.') }
@@ -108,12 +117,12 @@ end
 # end
 # end
 # end
-# 
+#
 # # PUT /discussions/1
 # # PUT /discussions/1.xml
 # def update
 # @discussion = Discussion.find(params[:id])
-# 
+#
 # respond_to do |format|
 # if @discussion.update_attributes(params[:discussion])
 # format.html { redirect_to(@discussion, :notice => 'Discussion was successfully updated.') }
@@ -124,13 +133,13 @@ end
 # end
 # end
 # end
-# 
+#
 # # DELETE /discussions/1
 # # DELETE /discussions/1.xml
 # def destroy
 # @discussion = Discussion.find(params[:id])
 # @discussion.destroy
-# 
+#
 # respond_to do |format|
 # format.html { redirect_to(discussions_url) }
 # format.xml  { head :ok }
