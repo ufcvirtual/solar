b92b3c16ce33257dfebb98d815d9ef3b22cc0a18
Humberto Gomes <humberto@virtual.ufc.br>
2011-06-02 16:31:00 -0300

    [#13646393] Fórum: Adicionando arquivos do ckeditor que faltaram no commit anterior

---
+++ public/javascripts/ckeditor/config.js
@@ -0,0 +1,26 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function (config) {
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
+
+    config.toolbar = 'SolarToolbar';
+
+    config.toolbar_SolarToolbar =
+	[
+		{ name: 'document', items: ['Source'] },
+	    { name: 'clipboard', items: ['Cut', 'Copy', 'Paste', '-', 'Undo', 'Redo'] },
+        { name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', ] },
+        '/',
+        { name: 'styles', items: ['Font', 'FontSize'] },
+	    { name: 'colors', items: ['TextColor', 'BGColor'] },
+        { name: 'basicstyles', items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat'] },
+        { name: 'insert', items: ['Image', 'Table', 'HorizontalRule', 'SpecialChar'] },
+	    { name: 'links', items: ['Link', 'Unlink'] },
+	    //{ name: 'tools', items: ['Maximize', 'ShowBlocks', '-', 'About'] }
+	];
+};
