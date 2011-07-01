7ab21e32d107a567405fa4dfa6f0eac52b13c6be
Laerte <laerte@virtual.ufc.br>
2011-07-01 15:45:09 -0300

    [#6560359] Colocando os seeds das agendas

--- app/controllers/agenda_controller.rb
+++ app/controllers/agenda_controller.rb
@@ -3,7 +3,8 @@ class AgendaController < ApplicationController
   def list
 
     @curriculum_unit = CurriculumUnit.find(params[:id])
-
+    @agendas = Agenda.find(1) #teste
+    
 #    # pegando dados da sessao e nao da url
 #    @groups_id = session[:opened_tabs][session[:active_tab]]["groups_id"]
 #    @offers_id = session[:opened_tabs][session[:active_tab]]["offers_id"]
