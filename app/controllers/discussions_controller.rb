a53b6253f26da5f71f3070ea46fa9d32d3920b1e
Bruno <bruno@centroproii14linux.instituto.ufcvirtual.br>
2011-07-21 17:19:13 -0300

    [#13646425] Regras de Negócios de postagem(So o dono apaga/atualiza, num periodo valido um post sem respostas)

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -3,6 +3,18 @@ class DiscussionsController < ApplicationController
   include DiscussionPostsHelper
 
   load_and_authorize_resource #Setar permissoes!!!!!
+  
+  def valid_date
+    @discussion.start <= Date.today && Date.today <= @discussion.end
+  end
+  
+  def owned_by_current_user
+      current_user.id == @discussion_post.user_id
+  end
+  
+  def has_no_response
+    DiscussionPost.find_all_by_father_id(discussion_post_id).empty?
+  end
 
   def list
 
@@ -54,22 +66,41 @@ class DiscussionsController < ApplicationController
     #DEFINIR O PROFILE!!!! 
     #profile_id    = 2
     profile_id    = 1
+   
+    @discussion= Discussion.find_by_id(discussion_id)
+    
+    #Usuário só pode criar posts no período ativo do fórum
+    if (valid_date)
+    
+       new_discussion_post = DiscussionPost.new :discussion_id => discussion_id, :user_id => current_user.id, :profile_id => profile_id, :content => content, :father_id => parent_id
+       new_discussion_post.save
 
-    new_discussion_post = DiscussionPost.new :discussion_id => discussion_id, :user_id => current_user.id, :profile_id => profile_id, :content => content, :father_id => parent_id
-    new_discussion_post.save
-
-    if @display_mode != "PLAINLIST"
-      hold_pagination
-    end
+       if @display_mode != "PLAINLIST"
+         hold_pagination
+       end
+    
+    end   
     redirect_to "/discussions/show/" << discussion_id
   end
 
   def remove_post
     discussion_id = params[:discussion_id]
     discussion_post_id = params[:discussion_post_id]
+    
+    #Relação de usuário com o post
+    #(# Só o dono do post pode apagar, se estiver no período ativo do fórum e se a postagem não tiver resposta(filhos))
+    
+    @discussion_post= DiscussionPost.find_by_id(discussion_post_id)
+    @discussion= Discussion.find_by_id(discussion_id)
+    
+    if (owned_by_current_user) &&
+        (valid_date)&&
+        (has_no_response)   
+      
     DiscussionPost.delete(discussion_post_id)
 
     hold_pagination
+    end
     redirect_to "/discussions/show/" << discussion_id
   end
 
@@ -78,10 +109,19 @@ class DiscussionsController < ApplicationController
     discussion_post_id = params[:discussion_post_id]
     new_content = params[:content]
 
+    #(# Só o dono do post pode editar, se estiver no período ativo do fórum e se a postagem não tiver resposta(filhos))
+    @discussion_post= DiscussionPost.find_by_id(discussion_post_id)
+    @discussion= Discussion.find_by_id(discussion_id)
+    
+    if (owned_by_current_user) &&
+        (valid_date)&&
+        (has_no_response)   
+        
     post = DiscussionPost.find(discussion_post_id);
     post.update_attributes({:content => new_content})
 
-    hold_pagination
+    hold_pagination 
+    end
     redirect_to "/discussions/show/" << discussion_id
   end
 
