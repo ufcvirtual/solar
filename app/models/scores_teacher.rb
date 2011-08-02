da24a0b7d91c09fdd0e13d8ac60784db6937f436
Wedson Lima <wedson.lima@virtual.ufc.br>
2011-08-02 10:50:24 -0300

    [#16464943] Dividindo as notas dos alunos no acompanhamento do professor

--- app/models/scores_teacher.rb
+++ app/models/scores_teacher.rb
@@ -3,36 +3,50 @@ class ScoresTeacher < ActiveRecord::Base
   set_table_name "assignment_comments"
 
   def self.list_students_by_group_id(group_id, page = 1)
-    paginate_by_sql "
-    WITH cte_private_files AS (
-       SELECT t1.id         AS user_id,
-              COUNT(t4.id)  AS cnt_private_files
-         FROM users             AS t1
-    LEFT JOIN send_assignments  AS t2 ON t2.user_id = t1.id
-    LEFT JOIN assignments       AS t3 ON t3.id = t2.assignment_id
-    LEFT JOIN assignment_files  AS t4 ON t4.send_assignment_id = t2.id
-        GROUP BY t1.id
+
+    sql = <<SQL
+    WITH cte_assignments AS (
+      SELECT t2.user_id,
+             translate(array_agg(
+             CASE
+                WHEN t2.grade IS NOT NULL THEN t2.grade::text
+                WHEN t3.id    IS NOT NULL THEN 'TE'
+                WHEN t3.id    IS NULL     THEN 'TN'
+             END
+             )::text, '{}', '') AS assignment_grades,
+             translate(array_agg(t2.assignment_id)::text,'{}','') AS assignment_ids
+        FROM assignments      AS t1
+        JOIN allocation_tags  AS t4 ON t4.id = t1.allocation_tag_id
+   LEFT JOIN send_assignments AS t2 ON t2.assignment_id = t1.id
+   LEFT JOIN assignment_files AS t3 ON t3.send_assignment_id = t2.id
+       WHERE t4.group_id = 3
+       GROUP BY t2.user_id
+       ORDER BY t2.user_id
     )
     --
       SELECT DISTINCT t1.id,
              t1.name,
              COUNT(t7.id) AS cnt_public_files,
-             t8.cnt_private_files
+             COALESCE(t8.assignment_grades, 'TN,TN') AS assignment_grades,
+             COALESCE(t8.assignment_ids, '2,3')      AS assignment_ids
         FROM users              AS t1
         JOIN allocations        AS t2 ON t2.user_id = t1.id
         JOIN profiles           AS t3 ON t3.id = t2.profile_id
         JOIN allocation_tags    AS t4 ON t4.id = t2.allocation_tag_id
         JOIN groups             AS t5 ON t5.id = t4.group_id
-        JOIN cte_private_files  AS t8 ON t8.user_id = t1.id
+   LEFT JOIN cte_assignments AS t8 ON t8.user_id = t1.id
    LEFT JOIN public_files       AS t7 ON t7.allocation_tag_id = t4.id AND t7.user_id = t1.id
        WHERE t3.student = TRUE
          AND t5.id = #{group_id}
-       GROUP BY t1.id, t1.name, t8.cnt_private_files
-       ORDER BY t1.name, t1.id",
-      {:per_page => Rails.application.config.items_per_page, :page => page}
+       GROUP BY t1.id, t1.name, t8.assignment_grades, t8.assignment_ids
+       ORDER BY t1.name, t1.id
+SQL
+
+    paginate_by_sql sql, {:per_page => Rails.application.config.items_per_page, :page => page}
 
   end
 
+  # numero de estudantes por group
   def self.number_of_students_by_group_id(group_id)
     cnt = ActiveRecord::Base.connection.select_all <<SQL
   SELECT COUNT(DISTINCT t1.id)::int AS cnt
