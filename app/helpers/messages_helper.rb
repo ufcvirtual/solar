890fac60a83b3f6a217f8340b1fa1e4dc68b16ac
Humberto Gomes <humberto@virtual.ufc.br>
2011-08-05 17:13:40 -0300

    [#master] Portlet de mensagens: Correção do quarto parâmetro do return_messages e links para caixa de entrada e nova mensagem

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,6 +1,7 @@
 module MessagesHelper
 
-  def return_messages(userid, type='index', tag=nil, search_text)
+  def return_messages (userid, type='index', tag=nil, search_text='')
+
     query_fields = "select m.*, usm.user_id, f.original_name, u.name, u.nick,ml.title label,
         cast( usm.status & '#{Message_Filter_Sender.to_s(2)}' as boolean)ehorigem,
         cast( usm.status & '#{Message_Filter_Read.to_s(2)}' as boolean)ehlida,
