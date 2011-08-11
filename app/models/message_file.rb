2e9ab8f600c26ba421c2c3884c5472cdd0e20143
patriciapaula <patricia@virtual.ufc.br>
2011-08-11 17:10:04 -0300

    [#13647207] trabalhando com os anexos...

--- app/models/message_file.rb
+++ app/models/message_file.rb
@@ -7,5 +7,5 @@ class MessageFile < ActiveRecord::Base
     :url => "/media/message/:id_:basename.:extension"
 
   validates_attachment_size :message, :less_than => 10.megabytes, :message => " " # Esse :message => " " deve permanecer dessa forma enquanto não descobrirmos como passar a mensagem de forma correta. Se o message for vazio a validação não é feita.
-  
+
 end
