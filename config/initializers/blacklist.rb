16095e6253f0b345e550239860397e91a0d6bfd1
Humberto Gomes <humberto@virtual.ufc.br>
2011-09-01 17:10:42 -0300

    [#17295421] Validador blacklist agora em config/initializers/blacklist.rb. Seguindo http://guias.rubyonrails.com.br/activerecord_validations_callbacks.html

--- config/initializers/blacklist.rb
+++ config/initializers/blacklist.rb
@@ -1,6 +1,3 @@
-# Inicializando uma nova validacao
-require File.join(::Rails.root.to_s, 'lib', 'validations', 'blacklist')
-
 #mudando armazenamento de sessao para o banco
 Solar::Application.config.black_list = [
   'application/x-asp',
@@ -9,3 +6,19 @@ Solar::Application.config.black_list = [
   'text/x-java',
   'application/x-java' # class
 ]
+
+ActiveRecord::Base.class_eval do
+      # Verifica se o content type do arquivo em questao se encontra na blacklist
+      # caso se encontre, sera lancado um erro
+      def self.validates_attachment_content_type_in_black_list name, options = {}
+        validation_options = options.dup
+        list = validation_options[:blacklist] || Solar::Application.config.black_list # se a lista nao for passada por parametro, é recuperada do ambiente
+        rejected_types = [list].flatten # transforma em um array simples
+        validates_each(:"#{name}_content_type", validation_options) do |record, attr, value|
+          if rejected_types.include?(value)
+            message = options[:message] || :invalid_type
+            record.errors.add(:"#{name}_content_type", message)
+          end
+        end
+      end
+end
