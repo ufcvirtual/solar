c0e8bda17a80da533d99b3989521ddc8a807e6f7
Wedson Lima <wedson.lima@virtual.ufc.br>
2011-09-12 14:52:08 -0300

    [#17888847] Melhorando consulta da agenda..considerando apenas os eventos criados para as ofertas e para os grupos

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -4,7 +4,7 @@ class Schedule < ActiveRecord::Base
   has_many :schedule_events
   has_many :portfolio
 
-  def self.all_by_curriculum_unit_id_and_offer_id_group_id_and_user_id(curriculum_unit_id, offer_id, group_id, user_id, period = false, date_search = nil)
+  def self.all_by_offer_id_and_group_id_and_user_id(offer_id, group_id, user_id, period = false, date_search = nil)
 
     date_search_option = ''
     date_search_option = "AND (t2.start_date = current_date OR t2.end_date = current_date)" if date_search.nil? && period
@@ -21,69 +21,54 @@ class Schedule < ActiveRecord::Base
         WHERE t2.user_id = #{user_id}
           AND t2.status = #{Allocation_Activated}
     ),
-    -- descobrir todas as ofertas desses curriculum_units dessas allocations
-    cte_offers_by_curriculum_unit AS (
-        SELECT t1.allocation_tag_id,
-               t1.curriculum_unit_id,
-               t2.id           AS offer_id
-          FROM cte_allocations AS t1
-          JOIN offers          AS t2 ON t2.curriculum_unit_id = t1.curriculum_unit_id
+    -- todas as ofertas a partir dos grupos
+    cte_offers_from_groups AS (
+       SELECT t2.offer_id
+         FROM cte_allocations   AS t1
+         JOIN groups            AS t2 ON t2.id = t1.group_id
     ),
-    -- descobrir todos os grupos dessas ofertas
-    cte_groups_by_offers_by_uc AS (
-        SELECT t1.allocation_tag_id,
-               t1.curriculum_unit_id,
-               t1.offer_id,
-               t2.id AS group_id
-          FROM cte_offers_by_curriculum_unit AS t1
-          JOIN groups                        AS t2 ON t2.offer_id = t1.offer_id
+    -- todos os grupos a partir da oferta
+    cte_groups_from_offers AS (
+        SELECT t3.id AS group_id
+          FROM cte_allocations  AS t1
+          JOIN offers           AS t2 ON t2.id = t1.offer_id
+          JOIN groups           AS t3 ON t3.offer_id = t2.id
     ),
-    -- todas as allocation_tags da unidade curricular
-    cte_allocations_by_uc AS (
-        SELECT t1.id           AS allocation_tag_id,
-               t1.curriculum_unit_id,
+    -- juncao das allocation_tags de groups e offers
+    cte_all_allocation_tags AS (
+     (
+        SELECT t1.id AS allocation_tag_id,
                t1.offer_id,
                t1.group_id
-          FROM allocation_tags AS t1
-         WHERE t1.group_id           IN (SELECT group_id FROM cte_groups_by_offers_by_uc)
-            OR t1.offer_id           IN (SELECT offer_id FROM cte_offers_by_curriculum_unit)
-            OR t1.curriculum_unit_id IN (SELECT curriculum_unit_id FROM cte_allocations)
-    ),
-    -- descobrir todos os grupos dessas ofertas
-    cte_groups_by_offers AS (
-        SELECT t1.allocation_tag_id,
-               t1.curriculum_unit_id,
-               t1.offer_id,
-               t2.id           AS group_id
-          FROM cte_allocations AS t1
-          JOIN groups          AS t2 ON t2.offer_id = t1.offer_id
-    ),
-    -- allocations by offers
-    cte_allocations_by_offers AS (
-        SELECT t1.id           AS allocation_tag_id,
-               t1.curriculum_unit_id,
-               t1.offer_id,
-               t1.group_id
-          FROM allocation_tags AS t1
-         WHERE t1.group_id IN (SELECT group_id FROM cte_groups_by_offers)
-            OR t1.offer_id IS NOT NULL
-     ),
-     cte_all_allocation_tags AS  (
-        SELECT * FROM cte_allocations_by_uc
-        UNION
-        SELECT * FROM cte_allocations_by_offers
-        UNION
-        SELECT * FROM cte_allocations WHERE group_id IS NOT NULL
-    )
--- consulta
+          FROM allocation_tags          AS t1
+          JOIN cte_offers_from_groups   AS t2 ON t2.offer_id = t1.offer_id
+     )
+     UNION
+     (
+        SELECT allocation_tag_id,
+               offer_id,
+               group_id
+          FROM cte_allocations
+         WHERE group_id IS NOT NULL OR offer_id IS NOT NULL
+     )
+     UNION
+     (
+         SELECT t1.id AS allocation_tag_id,
+                t1.offer_id,
+                t1.group_id
+           FROM allocation_tags AS t1
+           JOIN cte_groups_from_offers AS t2 ON t2.group_id = t1.group_id
+     )
+     )
+    -- consulta
    SELECT * FROM (
       (
         SELECT t1.name, t1.description, t2.start_date, t2.end_date , 'discussions' AS schedule_type
           FROM discussions             AS t1
           JOIN schedules               AS t2 ON t2.id = t1.schedule_id
           JOIN cte_all_allocation_tags AS t3 ON t3.allocation_tag_id = t1.allocation_tag_id
-         WHERE (t3.group_id = #{group_id} OR t3.offer_id = #{offer_id} OR t3.curriculum_unit_id = #{curriculum_unit_id})
-               #{date_search_option}
+         WHERE (t3.group_id = #{group_id} OR t3.offer_id = #{offer_id})
+           #{date_search_option}
       )
       UNION
       (
@@ -91,8 +76,8 @@ class Schedule < ActiveRecord::Base
           FROM lessons                 AS t1
           JOIN schedules               AS t2 ON t2.id = t1.schedule_id
           JOIN cte_all_allocation_tags AS t3 ON t3.allocation_tag_id = t1.allocation_tag_id
-         WHERE (t3.group_id = #{group_id} OR t3.offer_id = #{offer_id} OR t3.curriculum_unit_id = #{curriculum_unit_id})
-               #{date_search_option}
+         WHERE (t3.group_id = #{group_id} OR t3.offer_id = #{offer_id})
+           #{date_search_option}
       )
       UNION
       (
@@ -100,8 +85,8 @@ class Schedule < ActiveRecord::Base
         FROM assignments             AS t1
         JOIN schedules               AS t2 ON t2.id = t1.schedule_id
         JOIN cte_all_allocation_tags AS t3 ON t3.allocation_tag_id = t1.allocation_tag_id
-       WHERE (t3.group_id = #{group_id} OR t3.offer_id = #{offer_id} OR t3.curriculum_unit_id = #{curriculum_unit_id})
-             #{date_search_option}
+       WHERE (t3.group_id = #{group_id} OR t3.offer_id = #{offer_id})
+           #{date_search_option}
       )
       UNION
       (
@@ -109,14 +94,13 @@ class Schedule < ActiveRecord::Base
         FROM schedule_events         AS t1
         JOIN schedules               AS t2 ON t2.id = t1.schedule_id
         JOIN cte_all_allocation_tags AS t3 ON t3.allocation_tag_id = t1.allocation_tag_id
-       WHERE (t3.group_id = #{group_id} OR t3.offer_id = #{offer_id} OR t3.curriculum_unit_id = #{curriculum_unit_id})
-             #{date_search_option}
+       WHERE (t3.group_id = #{group_id} OR t3.offer_id = #{offer_id})
+           #{date_search_option}
       )
     ) AS t1
    ORDER BY t1.end_date
 
 SQL
-
   end
 
 end
