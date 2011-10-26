8012bdcce74db76da1399da8e1f02a9802490f71
Bruno <bruno@centroproii14linux.instituto.ufcvirtual.br>
2011-10-26 16:49:29 -0300

    [#13647383] Selenium rodando no servidor local

--- features/acessar_unidade_curricular.feature
+++ features/acessar_unidade_curricular.feature
@@ -9,6 +9,7 @@ Contexto:
         | 1        | 1                  | 3           | 1      |
         | 2        | 1                  | 2           | 1      |
 
+
 Cenário: Acessar pagina de unidade curricular
     Dado que estou logado com o usuario "user" e com a senha "user123"
         E que estou em "Meu Solar"
--- features/support/env.rb
+++ features/support/env.rb
@@ -13,6 +13,12 @@ require 'cucumber/rails'
 # steps to use the XPath syntax.
 Capybara.default_selector = :css
 
+Capybara.app_host = 'http://localhost:3000'
+
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+
 # By default, any exception happening in your Rails application will bubble up
 # to Cucumber so that your scenario will fail. This is a different from how 
 # your application behaves in the production environment, where an error page will 
