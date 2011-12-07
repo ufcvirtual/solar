986bd93773f55402151402068c2c8d58bddf1a14
Laerteufc <laerte@virtual.ufc.br>
2011-12-07 14:46:02 -0300

    [#21284625] Logica das pastas no upload do material de apoio

--- app/controllers/support_material_file_editor_controller.rb
+++ app/controllers/support_material_file_editor_controller.rb
@@ -38,10 +38,13 @@ class SupportMaterialFileEditorController < ApplicationController
   end
 
   def upload_files
-    #### PEGAR ALLOCATION TAG ! ! !
     #    authorize! :upload_files, SupportMaterialFileEditor
+
+    
+    #### PEGAR ALLOCATION TAG ! ! !
     allocation_tag_id = 3
 
+    
     respond_to do |format|
       begin
         # redireciona para a lista
@@ -53,12 +56,20 @@ class SupportMaterialFileEditorController < ApplicationController
         # allocation_tag do grupo selecionada
         #        allocation_tag_id = AllocationTag.find(:first, :conditions => ["group_id = ?", session[:opened_tabs][session[:active_tab]]["groups_id"]]).id
 
-
+        # Verificando se é uma pasta existente no banco ou uma nova criado pelo usuário.
         if (params[:support_material][:new_folder] != "")
           params[:support_material][:folder] = params[:support_material][:new_folder]
         end
-        
         params[:support_material].delete(:new_folder)
+        
+        # Verificando se o arquivo enviado já existe na pasta selecionada
+        file = SupportMaterialFile.new params[:support_material]
+  
+        # Se retornar um registro é pq ja existe no banco e nao pode inserir, se for vazio pode inserir
+        verify = SupportMaterialFile.find_by_attachment_file_name_and_folder(file.attachment_file_name, file.folder)
+        unless (verify.nil?)
+          raise "Arquivo escolhido existe nessa mesma pasta"
+        end
 
         @file = SupportMaterialFile.new params[:support_material]
         @file.allocation_tag_id = allocation_tag_id
