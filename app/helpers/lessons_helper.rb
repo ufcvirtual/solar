33618ae2c0aebb26c05f9df02a70d01c7b249392
Bruno Neves <bruno@virtual.ufc.br>
2012-01-26 15:30:07 -0300

    [#21472023] Retirando visualização de aulas inativas ou expiradas e mudando a cor e retirando os links das mesmas

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -7,10 +7,14 @@ module LessonsHelper
 
     lessons.each do |l|
       order_lesson = (atual_id_open == l.lesson_id) ? count.to_s : order_lesson.to_s
-
+       
+     
       text_lesson = t(:lesson) + ' ' + count.to_s + ' - ' + total_lesson.to_s
-      text += "<span class='lesson_link' id='lesson_link#{count.to_s}' onclick=javascript:reload_frame('#{l.address}','#{URI.escape(text_lesson)}','#{count.to_s}')>"+count.to_s+"</span>"
-
+       
+       unless (l.schedule.end_date < Date.today || l.schedule.start_date > Date.today)
+          text += "<span class='lesson_link' id='lesson_link#{count.to_s}' onclick=javascript:reload_frame('#{l.address}','#{URI.escape(text_lesson)}','#{count.to_s}')>"+count.to_s+"</span>"
+       end
+       
       count = count + 1
     end
 
