ab837d3be62946c2fda538b8ad46e1ae4b647bf3
Ricardo Palácio <ricardo@virtual.ufc.br>
2012-02-02 17:54:18 -0300

    [#24375731] Corrigido...

--- app/models/user.rb
+++ app/models/user.rb
@@ -84,7 +84,8 @@ class User < ActiveRecord::Base
 
   # Alocar usuario para acesso com o perfil básico no ato da sua criação
   def basic_profile_allocation
-    new_allocation_user = Allocation.new :profile_id => Profiles.find_by_type(Profile_Type_Basic), :status => Allocation_Activated, :user_id => self.id
+    puts "\n\n\n\nProfile.find_by_types(Profile_Type_Basic):#{Profile.find_by_types(Profile_Type_Basic)}\n\n\n\n"
+    new_allocation_user = Allocation.new :profile_id => Profile.find_by_types(Profile_Type_Basic).id, :status => Allocation_Activated, :user_id => self.id
     new_allocation_user.save!
   end
 
