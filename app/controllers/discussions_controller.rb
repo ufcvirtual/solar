ae97caf39557c7f05171a0b8bc9f02bafcae43ae
biancastephani <bianca.stephani@gmail.com>
2012-03-28 11:18:04 -0300

    [#26726493] A listagem dos foruns era feita para determinada allocation_tag e determinado grupo (turma) selecionado. Se esse grupo vinha vazio, ele exibia todos os fóruns de todos os grupos daquele usuário. Alterei a pesquisa para pesquisar apenas de acordo com o grupo atual (caso não tenha nenhum selecionado, pega o primeiro grupo que existe para o usuário). Sabendo-se que a visualização de fóruns é de acordo com cada grupo, independente do perfil do usuário (ou seja, se eu vou ver um fórum, irei ver o o fórum para um grupo específico).

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -11,10 +11,16 @@ class DiscussionsController < ApplicationController
   def list
     authorize! :list, Discussion
 
-    allocation_tag_id = active_tab[:url]['allocation_tag_id']
-    allocations = AllocationTag.find_related_ids(allocation_tag_id)
-    @discussions = Discussion.all_by_allocations(allocations.join(','))
+    curriculum_unit_id = active_tab[:url]['id']
 
+    # O grupo (turma) a ter seus fóruns exibidos será o grupo selecionado na aba de seleção ('selected_group')
+    group_selected = AllocationTag.find(active_tab[:url]['allocation_tag_id']).group_id
+    # Se o group_select estiver vazio, ou seja, nenhum grupo foi selecionado pelo usuário,
+    # o grupo a ter seus fóruns exibidos será o primeiro grupo encontrado para o usuário em questão
+    group_selected = CurriculumUnit.find_user_groups_by_curriculum_unit(curriculum_unit_id, current_user.id).first.id if group_selected.blank?
+
+    allocation_group = AllocationTag.all(:conditions => ["group_id = #{group_selected}"], :select => 'id').map( &:id )
+    @discussions = Discussion.all_by_allocations(allocation_group.join(','))
   end
 
   def show
