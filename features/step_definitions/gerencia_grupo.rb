cc09dccb95d4766cee8826f7fd704966935be229
patriciapaula <patricia@virtual.ufc.br>
2012-04-13 11:44:44 -0300

    [#26814763] fechando 1a tela de gerencia de grupo...

---
+++ features/step_definitions/gerencia_grupo.rb
@@ -0,0 +1,7 @@
+Capybara.add_selector(:element) do
+  xpath { |locator| "//*[normalize-space(text())=#{XPath::Expression::StringLiteral.new(locator)}]" }
+end
+
+Quando 'eu clicar no item "$locator"' do |locator|
+  find(:xpath, XPath::HTML.content(locator)).click
+end
