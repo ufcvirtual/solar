cc4b4122c8af4adcadd0c6878d10cd2a9af5c292
andreibosco <andreibosco@virtual.ufc.br>
2012-04-18 15:59:48 -0300

    [#28120519] corrigindo xpath do teste de matricula

--- features/step_definitions/matricula_steps.rb
+++ features/step_definitions/matricula_steps.rb
@@ -1,6 +1,6 @@
 Entao /^eu deverei ver a linha de opcao de matricula$/ do |tabela|
 	tabela.hashes.each do |linha|
-	    xpath = "//table/tr[ child::td[contains(., '#{linha[:UnidadeCurricular]}')]  and child::td[contains(., '#{linha[:Categoria]}')]  and child::td[contains(., '#{linha[:Turma]}')]  and   (descendant::input[@value='#{linha[:Matricula]}'] or child::td[contains(., '#{linha[:Matricula]}')])       ]"
+	    xpath = "//table/tbody/tr[ child::td[contains(., '#{linha[:UnidadeCurricular]}')]  and child::td[contains(., '#{linha[:Categoria]}')]  and child::td[contains(., '#{linha[:Turma]}')]  and   (descendant::input[@value='#{linha[:Matricula]}'] or child::td[contains(., '#{linha[:Matricula]}')])       ]"
 	    page.should have_xpath(xpath)
 	end
 end
@@ -31,7 +31,7 @@ Entao /^eu nao deverei visualizar a linha de opcao de matricula$/ do |tabela|
 end
 
 Quando /^eu clicar na opcao "([^"]*)" do item de matricula "([^"]*)"$/ do |link, texto|
-  xpath = "//table/tr[ child::td[contains(.,'#{texto}')] ]"
+  xpath = "//table/tbody/tr[ child::td[contains(.,'#{texto}')] ]"
   within(:xpath, xpath) do
     find_button("#{link}").click
   end
