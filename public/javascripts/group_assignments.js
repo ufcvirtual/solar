ce1c81ef5ddc1ff1b927cbc43ad27dd770d2ecee
biancastephani <bianca.stephani@gmail.com>
2012-06-01 12:32:45 -0300

    [#26814905] Refatoração do código javascript

---
+++ public/javascripts/group_assignments.js
@@ -0,0 +1,23 @@
+ function flash_message(msg, css_class) {
+    if ($('#flash_message')) { $('#flash_message').remove(); }
+    var html = '<div id="flash_message" class="' + css_class + '"><span>' + msg + '</span></div>';
+    $('.flash_message_wrapper').append(html);
+  }
+
+  function clickOnGroup(group_div_id){
+    this_div = $('#students_'+group_div_id);
+    if (this_div.css('display') == 'none'){
+      this_div.slideDown(); 
+      this_div.parents('li').find('.menu_icon_arrow').addClass('menu_icon_animate');
+    }else{
+      this_div.slideUp(); 
+      this_div.parents('li').find('.menu_icon_animate').removeClass('menu_icon_animate');
+    }
+  }
+
+  function changeOpendedGroupArrow(group_assignment_id){
+    this_div = $('#students_'+group_assignment_id);
+    if (this_div.css('display') == 'block'){
+      this_div.parents('li').find('.menu_icon_arrow').addClass('menu_icon_animate');
+    }
+  }
