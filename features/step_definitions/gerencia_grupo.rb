dee6fe8382df65d791850d585616a995dc69b82b
biancastephani <bianca.stephani@gmail.com>
2012-06-13 14:44:23 -0300

    [#30912953] Teste no acesso à página de novo grupo, correção de problemas nas permissões ao clicar em 'novo grupo'. Teste de cadastro de grupo, correção no problema na criação de grupo em que havia o 'submit' do formulario duas vezes, gerando um erro na segunda

--- features/step_definitions/gerencia_grupo.rb
+++ features/step_definitions/gerencia_grupo.rb
@@ -4,4 +4,9 @@ end
 
 Quando 'eu clicar no item "$locator"' do |locator|
   find(:xpath, XPath::HTML.content(locator)).click
+  page.execute_script("toggle_div('div_group_4')")
+end
+
+Entao /^deverei ir para "([^"]*)"$/ do |page_name|
+  visit path_to(page_name)
 end
