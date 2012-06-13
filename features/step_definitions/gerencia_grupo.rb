1f37756a4d9ee3f1e5f3c040b502755b3ed06347
biancastephani <bianca.stephani@gmail.com>
2012-06-13 16:13:32 -0300

    [#30912953] Teste de edição de grupo melhorado

--- features/step_definitions/gerencia_grupo.rb
+++ features/step_definitions/gerencia_grupo.rb
@@ -7,10 +7,14 @@ Quando 'eu clicar no item "$locator"' do |locator|
   page.execute_script("toggle_div('div_group_4')")
 end
 
-E 'que eu cliquei no grupo "$grupo_id"' do |grupo_id|
+Quando 'eu clicar no grupo "$grupo_id"' do |grupo_id|
 	page.execute_script("clickOnGroup("+grupo_id+")")
 end
 
 Quando 'eu selecionar o usuario de id "$id"' do |id|
 	check('students__'+id)
+end
+
+Entao 'eu deverei ver os alunos do grupo com id "$id" selecionados' do |id|
+	find('#students__'+id).should be_checked
 end
