11c6320127bed5d7546dbed3fe0c661b7af1b658
Humberto Gomes <humberto@virtual.ufc.br>
2012-06-14 17:51:03 -0300

    [#28952045] Nome de variável

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -4,8 +4,8 @@ class DiscussionsController < ApplicationController
     authorize! :index, Discussion
 
     begin
-      at_id = (active_tab[:url].include?('allocation_tag_id')) ? active_tab[:url]['allocation_tag_id'] : AllocationTag.find_by_group_id(params[:group_id] || []).id
-      @discussions = Discussion.all_by_allocation_tags(AllocationTag.find_related_ids(at_id))
+      allocation_tag_id = (active_tab[:url].include?('allocation_tag_id')) ? active_tab[:url]['allocation_tag_id'] : AllocationTag.find_by_group_id(params[:group_id] || []).id
+      @discussions = Discussion.all_by_allocation_tags(AllocationTag.find_related_ids(allocation_tag_id))
     rescue
       @discussions = []
     end
