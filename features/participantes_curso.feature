179a6cdf938e645bca5e2f64f0e5433f88fbbbce
Wedson Lima <wedson.sousa.lima@gmail.com>
2012-09-14 12:27:36 -0300

    [#29173089] Testes funcionais para gerencia de matricula

--- features/participantes_curso.feature
+++ features/participantes_curso.feature
@@ -1,4 +1,5 @@
 # language: pt
+
 Funcionalidade: Exibir participantes do curso
   Como um usuario do solar
   Eu quero visualizar os participantes do curso
@@ -13,17 +14,17 @@ Dado que tenho "allocations"
 |    7    |         2         |     5      |    1   |
 |    7    |         3         |     5      |    1   |
 
-  @javascript
-  Cenário: Visualizar participantes da turma
-    Dado que estou logado com o usuario "user" e com a senha "123456"
-        E que estou em "Meu Solar"
-    Quando eu clicar no link "Introducao a Linguistica"
-        E que eu selecionei "selected_group" com "FOR - 2011.1"
-        E eu clicar no link "Informações Gerais"
-    Então eu deverei ver "Participantes"
-    Quando eu clicar no link "Participantes"
-        Então eu deverei ver "Responsáveis"
-        E eu deverei ver "Usuario do Sistema"
-        E eu deverei ver "Participantes da turma"
-        E eu deverei ver "Ricardo Palacio"
-        E eu deverei ver "Bezerra"
+@javascript
+Cenário: Visualizar participantes da turma
+  Dado que estou logado com o usuario "user" e com a senha "123456"
+    E que estou em "Meu Solar"
+  Quando eu clicar no link "Introducao a Linguistica"
+    E que eu selecionei "selected_group" com "FOR - 2011.1"
+    E eu clicar no link "Informações Gerais"
+  Então eu deverei ver "Participantes"
+  Quando eu clicar no link "Participantes"
+    Então eu deverei ver "Responsáveis"
+    E eu deverei ver "Usuario do Sistema"
+    E eu deverei ver "Participantes da turma"
+    E eu deverei ver "User 2"
+    E eu deverei ver "User 3"
--- test/functional/allocations_controller_test.rb
+++ test/functional/allocations_controller_test.rb
@@ -4,30 +4,101 @@ class AllocationsControllerTest < ActionController::TestCase
 
   include Devise::TestHelpers
 
-  test "listagem de pedidos de matricula por um usuario com permissao" do
-    sign_in users(:coorddisc)
+  def setup
+    @coordenador = users(:coorddisc)
+    sign_in @coordenador
+  end
 
+  test "listar pedidos de matricula por um usuario com permissao" do
     get :index
     assert_response :success
     assert_not_nil assigns(:allocations)
   end
 
-  test "listagem de pedidos de matricula por um usuario sem permissao" do
+  test "listar pedidos de matricula por um usuario sem permissao" do
+    sign_out @coordenador
     sign_in users(:aluno1)
 
     get :index
     assert_response :redirect
   end
 
-  test "seletor" do
-    sign_in users(:coorddisc)
-
+  test "exibir quantidade de alunos listados" do
     get :index
     assert_response :success
 
+    assert_tag :select, :attributes => {:id => "status_id"}, :content => /Todos/
     assert_select '.filter_counter', "(Total: 4 alunos)"
-    # assert_select '#status_id', "Pendente"
   end
 
+  # turma que o coordenador nao coordena
+  test "nao exibir pedidos de matricula da turma CAU-A" do
+    get :index
+    assert_response :success
+
+    assert_select "table tbody tr" do
+      assert_select 'td:nth-child(3)', {:count => 0, :html => "CAU-A - 2011.1"}
+    end
+  end
+
+  test "aceitar matricula pendente do aluno user" do
+    get :index
+    assert_response :success
+
+    al_pending = assigns(:allocations).select {|al| al.user_id == users(:user).id and al.status == Allocation_Pending}.first
+
+    assert_select "table tbody tr" do
+      assert_select 'td:nth-child(4)', {:count => 1, :html => "Pendente"}
+    end
+
+    get :edit, :id => al_pending.id
+    assert_response :success
+
+    put :update, {:id => al_pending.id, :allocation => {:status => Allocation_Activated}}
+    assert_response :success
+
+    assert_select 'td', {:html => "Matriculado"}
+  end
+
+  test "rejeitar matricula pendente do aluno user" do
+    get :index
+    assert_response :success
+
+    al_pending = assigns(:allocations).select {|al| al.user_id == users(:user).id and al.status == Allocation_Pending}.first
+
+    assert_select "table tbody tr" do
+      assert_select 'td:nth-child(4)', {:count => 1, :html => "Pendente"}
+    end
+
+    get :edit, :id => al_pending.id
+    assert_response :success
+
+    put :update, {:id => al_pending.id, :allocation => {:status => Allocation_Rejected}}
+    assert_response :success
+
+    assert_select 'td', {:html => "Rejeitado"}
+  end
+
+  test "cancelar matricula do aluno user na turma FOR - 2011.1" do
+    get :index
+    assert_response :success
+
+    al_matriculado = assigns(:allocations).select {|al| al.user_id == users(:user).id and al.status == Allocation_Activated and al.allocation_tag_id == 1}
+
+    # o aluno user esta matriculado na turma?
+    assert (not al_matriculado.empty?)
+
+    get :edit, :id => al_matriculado.first.id
+    assert_response :success
+
+    put :update, {:id => al_matriculado.first.id, :allocation => {:status => Allocation_Cancelled}}
+    assert_response :success
+
+    assert_select 'td', {:html => "Cancelado"}
+  end
+
+  test "mudar aluno de turma" do
+    pending "mudar aluno de turma"
+  end
 
 end
