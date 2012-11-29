e5eb86cc132654a42c5bc3115824520afa953278
Wedson Lima <wedson.sousa.lima@gmail.com>
2012-11-29 10:25:06 -0300

    [#39887437] Novo metodo para compactacao de arquivos. Aceitas path do sistema de arquivos e faz zip do que estiver dentro.

--- app/helpers/files_helper.rb
+++ app/helpers/files_helper.rb
@@ -2,73 +2,85 @@ module FilesHelper
 
   def download_file(redirect_error, pathfile, filename = nil)
     if File.exist?(pathfile)
-      send_file pathfile, :filename => filename
+      send_file pathfile, filename: filename
     else
-      respond_to do |format|
-        format.html { redirect_to redirect_error, :alert => t(:file_error_nonexistent_file) }
-      end
+      redirect_to redirect_error, alert: t(:file_error_nonexistent_file)
     end
   end
 
-  ##
-  # Cria zip de arquivos com folders internos e retorna o path do zip
-  #
-  # Obs.: O zip será gerado com um hash. Se o um arquivo com um mesmo hash já existir ele não será criado, apenas o path será retornado.
-  #
-  # Parameters:
-  # - table_column_name: Coluna da tabela que identifica o nome do arquivo.
-  # - zip_name_folder: Nome do folder principal do zip. Se nao for informado os arquivos serão criados sem diretório.
-  ##
+  ## DEPRECATED: use compress
   def make_zip_files(files, table_column_name, zip_name_folder = nil)
+    compress({ files: files, table_column_name: table_column_name, name_zip_file: zip_name_folder })
+  end
+
+  ##
+  # {
+  #   files: ['objfile1', 'objfile2', 'objfile3'],
+  #   table_column_name: 'attachment'
+  #   name_zip_file: 'ziped_file'
+  # }
+  # or
+  # {
+  #   under_path: ['/path/to/1', '/path/to/2'],
+  #   name_zip_file: 'ziped_file'
+  # }
+  ##
+  def compress(opts = {})
     require 'zip/zip'
 
-    all_file_names = files.collect{|file| [file[table_column_name]]}.flatten
-    hash_name = Digest::SHA1.hexdigest(all_file_names.to_s)
-    zip_name_folder = hash_name if zip_name_folder.nil?
+    ## arquivos e o caminho principal nao foram indicados
+    return if not(opts[:files].present?) and not(opts[:under_path].present?)
 
-    # hash que será usado como nome do arquivo zip
-    all_files_ziped_name = File.join('tmp', "#{hash_name}.zip")
+    archive = File.join(Rails.root.to_s, 'tmp', '%s') << '.zip'
+    if not(opts[:files].present?) # under_path present
+      name_zip_file = opts[:name_zip_file].present? ? opts[:name_zip_file] : Digest::SHA1.hexdigest(opts[:under_path].join)
+      archive       = archive % name_zip_file
+      paths         = [opts[:under_path]].flatten.compact.uniq
 
-    ##
-    # Verifica se já existe um arquivo zipado na tmp com o mesmo conteúdo.
-    # Assim não será necessário recriar o zip, o usuário faz apenas download.
-    ##
-    existing_zip_files = Dir.glob('tmp/*') # lista dos arquivos .zip existentes no '/tmp'
-    zip_created = existing_zip_files.include?(all_files_ziped_name) # informa se ja existe o arquivo de zip desejado
-    name_internal_folder = ''
+      FileUtils.rm archive, force: true
+      Zip::ZipFile.open(archive, Zip::ZipFile::CREATE) do |zipfile|
+        paths.each do |path|
+          dir = path.split('/').last
+          zipfile.mkdir(dir)
+          Dir["#{path}/**/**"].each do |file|
+            zipfile.add(File.join(dir, file.sub(path + '/', '')), file) # nome do arquivo, path do arquivo
+          end # dir
+        end # each
+      end # zip
+    else # :files esta presente
+      ## objeto do banco de dados
+      if opts[:files].first.respond_to?(opts[:table_column_name].to_sym)
+        name_zip_file = Digest::SHA1.hexdigest(opts[:files].map(&opts[:table_column_name].to_sym).flatten.compact.sort.join)
+        archive       = archive % name_zip_file
 
-    # cria o zip caso nao esteja criado
-    unless zip_created
-      Zip::ZipFile.open(all_files_ziped_name, Zip::ZipFile::CREATE) do |zipfile|
-        files.each do |file|
-          unless file.attachment_file_name.nil?
-            unless zip_created
-              zipfile.mkdir(zip_name_folder) # cria folder geral para colocar todos os outros folders dentro dele
-              zip_created = true
-            end
+        return archive if File.exists?(archive)
 
-            # cria um novo folder interno
-            unless !file.attributes.include?('folder') or file.folder.nil?
-              if name_internal_folder != file.folder and file.folder != ''
-                name_internal_folder = file.folder
-                zipfile.mkdir(File.join(zip_name_folder, name_internal_folder))
-              end
-            end
+        Zip::ZipFile.open(archive, Zip::ZipFile::CREATE) do |zipfile|
+          make_tree(opts[:files]).each do |dir, files|
+            zipfile.mkdir(dir.to_s)
+            files.map { |file| zipfile.add(File.join(dir.to_s, file.attachment_file_name), file.attachment.path.to_s) if File.exists?(file.attachment.path.to_s) }
+          end # each
+        end # zip
+      end # if
+    end # if
 
-            zipfile.add(File.join(zip_name_folder, name_internal_folder, file.attachment_file_name), file.attachment.path) if File.exists?(file.attachment.path.to_s)
-          end
-        end
-      end
-    end
+    return archive
+  end # compress
 
-    unless zip_created
-      # recupera arquivo
-      zip_name = Zip::ZipFile.open(hash_name + ".zip", Zip::ZipFile::CREATE).to_s
-      path_zip = File.join(Rails.root.to_s, 'tmp', zip_name)
-    else
-      pathfile = "#{Rails.root}/#{all_files_ziped_name}"
-      send_file pathfile, :filename => all_files_ziped_name
-    end
+  def extract
+    raise "not done yet"
   end
+
+  private
+
+    ## files with folder
+    def make_tree(files)
+      tree = {}
+      files.each do |file|
+        tree[file.folder.to_sym] = [] unless tree[file.folder.to_sym].present?
+        tree[file.folder.to_sym] << file if file.respond_to?(:folder) and not(file.attachment_file_name.nil?)
+      end # each
+      tree.delete_if { |k,v| v.empty? }
+    end
 
 end
