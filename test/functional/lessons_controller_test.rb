b085bb78e5cd8c453976ce2e768cf9170e4b709b
Wedson Lima <wedson.sousa.lima@gmail.com>
2012-12-07 15:40:32 -0300

    [#40373189] Revisando testes de aulas e arquivos.

--- test/functional/lessons_controller_test.rb
+++ test/functional/lessons_controller_test.rb
@@ -1,10 +1,9 @@
 require 'test_helper'
+require 'zip/zip'
 
 class LessonsControllerTest < ActionController::TestCase
 
   include Devise::TestHelpers
-
-  # fixtures :lessons, :allocation_tags
 
   def setup
     @editor    = users(:editor)
@@ -12,10 +11,6 @@ class LessonsControllerTest < ActionController::TestCase
     @coordenador = users(:coorddisc)
     sign_in @editor
   end
-
-  ##
-  # Download_files
-  ##
 
   # Usuário com permissão
   test "permitir zipar e realizar download de arquivos de aulas" do
@@ -42,34 +37,20 @@ class LessonsControllerTest < ActionController::TestCase
     assert not(File.exists?(File.join(Rails.root.to_s, 'tmp', zip_name)))
   end
 
-  # Usuário com permissão, mas sem acesso às aulas selecionadas # INICIAR
-  # test "nao permitir zipar e realizar download de arquivos de aulas se nenhuma for selecionada" do
-  #   lessons_ids = []
+  # Usuário sem permissão
+  test "nao permitir zipar e realizar download de arquivos de aulas para usuario sem permissao" do
+    sign_in @professor
 
-  #   get(:download_files, {:lessons_ids => lessons_ids, :allocation_tags_ids => [allocation_tags(:al6)]})
-  #   # assert_template :list
-  #   # assert_redirected_to({:controller => :lessons, :action => :list, :allocation_tag_id => assigns(:allocation_tags_ids)})
-  #   # assert_redirected_to( list_lessons_url(:allocation_tag_id => assigns(:allocation_tags_ids)) )
-  #   assert_equal flash[:alert], I18n.t(:must_select_lessons, :scope => [:lessons, :notifications])
+    lessons_ids = [lessons(:pag_ufc).id.to_s, lessons(:pag_uol).id.to_s]
+    zip_name = Digest::SHA1.hexdigest(lessons_ids.collect{ |lesson_id| File.join(Rails.root.to_s, 'media', 'lessons', lesson_id) }.join) << '.zip'
+    FileUtils.rm File.join(Rails.root.to_s, 'tmp', zip_name), force: true ## deleta arquivo para testar se foi criado
 
-  #   zip_name = Digest::SHA1.hexdigest(lessons_ids.collect{ |lesson_id| File.join(Rails.root.to_s, 'media', 'lessons', lesson_id) }.join) << '.zip'
-  #   assert (not File.exists?(File.join(Rails.root.to_s, 'tmp', zip_name)))
-  # end
+    get(:download_files, {:lessons_ids => lessons_ids, :allocation_tags_ids => [allocation_tags(:al6)]})
+    assert_redirected_to home_path
+    assert_equal I18n.t(:no_permission), flash[:alert]
 
-  # # Usuário sem permissão # CONCLUIR
-  # test "nao permitir zipar e realizar download de arquivos de aulas para usuario sem permissao" do
-  #   sign_out @editor
-  #   sign_in @professor
-
-  #   lessons_ids = [lessons(:pag_ufc).id.to_s, lessons(:pag_uol).id.to_s]
-
-  #   get(:download_files, {:lessons_ids => lessons_ids, :allocation_tags_ids => [allocation_tags(:al6)]})
-  #   assert_redirected_to({:controller => :home})
-  #   assert_equal I18n.t(:no_permission), flash[:alert]
-
-  #   zip_name = Digest::SHA1.hexdigest(lessons_ids.collect{ |lesson_id| File.join(Rails.root.to_s, 'media', 'lessons', lesson_id) }.join) << '.zip'
-  #   assert (not File.exists?(File.join(Rails.root.to_s, 'tmp', zip_name)))
-  # end
+    assert not(File.exists?(File.join(Rails.root.to_s, 'tmp', zip_name)))
+  end
 
   test "exibir cadastro de modulos e aulas para um usuario com permissao" do
     sign_in @coordenador
@@ -79,23 +60,21 @@ class LessonsControllerTest < ActionController::TestCase
   end
 
   test "nao exibir cadastro de modulos e aulas para um usuario sem permissao" do
-    sign_out @editor
     sign_in users(:user2)
 
     get :list, { :allocation_tag_id => allocation_tags(:al5).id }
     assert_nil assigns(:lesson_modules)
-    assert_redirected_to({:controller => :home})
+    assert_redirected_to(home_path)
     assert_equal( flash[:alert], I18n.t(:no_permission) )
   end
 
-  test "rota para extrair arquivos de ual" do
+  test "rota para extrair arquivos de aula" do
     assert_routing extract_file_lesson_path("1", "file", "zip"), {
       controller: "lessons", action: "extract_files", id: "1", file: "file", extension: "zip"
     }
   end
 
   test "extrair arquivo de aula" do
-    require 'zip/zip'
     sign_in @coordenador
 
     lesson_id = lessons(:pag_goo).id.to_s
