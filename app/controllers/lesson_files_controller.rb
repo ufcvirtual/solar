70e55be2d11ce17b4b8148c809c7d48e3470e840
Wedson Lima <wedson.sousa.lima@gmail.com>
2012-12-13 12:00:58 -0300

    [#39822589] Listando arquivos da aula passada por parametro.

--- app/controllers/lesson_files_controller.rb
+++ app/controllers/lesson_files_controller.rb
@@ -1,12 +1,10 @@
 class LessonFilesController < ApplicationController
 
   def index
+    ## verificar se a lesson existe
     # lesson = Lesson.find(params[:lesson_id])
-    # file = File.join(Lesson::FILES_PATH, params[:lesson_id])
-
-    # if lesson.type_lesson == 1
-    #   raise "#{lesson.address}"
-    # end
+    file = File.join(Lesson::FILES_PATH, params[:lesson_id])
+    @files = directory_hash(file, 'RAIZ').to_json
   end
 
   def show
@@ -23,5 +21,22 @@ class LessonFilesController < ApplicationController
 
   def destroy
   end
+
+  private
+    def directory_hash(path, name=nil)
+      data = {title: (name || path)}
+      data[:children] = children = []
+      Dir.foreach(path) do |entry|
+        next if (entry == '..' || entry == '.')
+        full_path = File.join(path, entry)
+        if File.directory?(full_path)
+          data[:isFolder] = true
+          children << directory_hash(full_path, entry)
+        else
+          children << entry
+        end
+      end
+      return data
+    end
 
 end
