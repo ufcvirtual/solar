c668d84fc1e6a22048fb1fd2ee61b4260f2495ff
Andrei Bosco <andreibosco@virtual.ufc.br>
2013-03-07 16:47:30 -0300

    [#45794745] criando javascript de configuracao do ckeditor para definir botões da toolbar

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,12 @@
+CKEDITOR.editorConfig = function( config )
+{
+    config.toolbar =
+    [
+        { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+        { name: 'styles', items : [ 'Styles','Format' ] },
+        { name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
+        { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent', ] },
+        { name: 'links', items : [ 'Link','Unlink' ] },
+        { name: 'tools', items : [ 'Maximize' ] }
+    ];
+}
