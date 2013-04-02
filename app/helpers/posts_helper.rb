ef9a5ab6fbb95c21d768830573891f10769abfbd
Andrei Bosco <andreibosco@virtual.ufc.br>
2013-04-02 16:49:41 -0300

    [#43309193] corrigindo funcionamento de reply e edicao no forum, removendo underline dos icones de mensagem da pagina de programa e de remover arquivo anexo das paginas de forum

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -55,7 +55,7 @@ HTML
     files.each do |file|
       link_to_down   = (link_to file.attachment_file_name, download_post_post_file_path(post, file))
       link_to_remove = (editable and can_interact) ? (link_to (content_tag(:i, nil, :class=>'icon-cross-3 warning')), 
-        post_post_file_path(post, file), :confirm => t(".remove_file_confirm"), :method => :delete, :title => t(".remove_file"), 'data-tooltip' => t(".remove_file")) : ''
+        post_post_file_path(post, file), :confirm => t(".remove_file_confirm"), :method => :delete, :title => t(".remove_file"), 'data-tooltip' => t(".remove_file"), :class=>'nodecoration') : ''
       html_files << '<li>'
       html_files <<     "#{link_to_down}&nbsp;&nbsp;#{link_to_remove}"
       html_files << '</li>'
