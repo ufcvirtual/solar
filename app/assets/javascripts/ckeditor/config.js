9a837fa00d837718b688e32c0eab11ecea6f9879
Andrei Bosco <andreibosco@gmail.com>
2013-04-18 14:46:06 -0300

    [#47638339] removendo botão expandir da barra do ckeditor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -6,7 +6,6 @@ CKEDITOR.editorConfig = function( config )
         { name: 'styles', items : [ 'Styles','Format' ] },
         { name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
         { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent', ] },
-        { name: 'links', items : [ 'Link','Unlink' ] },
-        { name: 'tools', items : [ 'Maximize' ] }
+        { name: 'links', items : [ 'Link','Unlink' ] }
     ];
 }
