226597ff5df7bde7046e50e269882d4f262d2d93
biancastephani <bianca.stephani@gmail.com>
2013-05-09 11:54:40 -0300

    [#49218133] Remoção de código não mais utilizado / Re-adição das labels informativas de uc e curso no formulário

--- app/helpers/offers_helper.rb
+++ app/helpers/offers_helper.rb
@@ -1,63 +1,5 @@
 module OffersHelper
 
-	##
-  # Método que recupera as ofertas a serem listadas a partir dos valores de curso e uc selecionados no filtro (curriculum_unit_id e course_id)
-  # curriculum_unit_id e course_id podem ser 0, "all" ou algum id específico.
-  # 0, correspondente à seleção de "Nenhum" no filtro, não é aceito para a pesquisa de ofertas
-  ##
-  def get_offers(curriculum_unit_id, course_id)
-
-    get_curriculum_units_and_courses(curriculum_unit_id, course_id) # retorna lista de cursos e ucs que usuário tem acesso
-
-    # se algum valor for zero, a lista de ofertas será vazia
-    unless course_id == 0 or curriculum_unit_id == 0
-      all_offers      = []
-      course          = Course.find(course_id) unless course_id == "all"
-      curriculum_unit = CurriculumUnit.find(curriculum_unit_id) unless curriculum_unit_id == "all"
-
-      if course_id == "all" # foram selecionados todos os cursos
-
-        if curriculum_unit_id == "all" # e todas as uc
-          all_offers << @curriculum_units.collect { |curriculum_unit| curriculum_unit.offers }
-          all_offers << @courses.collect { |course| course.offers }
-        else # e uma uc específico
-          all_offers << curriculum_unit.offers
-          all_offers << @courses.collect { |course| course.offers }
-        end
-
-      else # foi selecionado um curso específico
-
-        if curriculum_unit_id == "all" # e todas as uc
-          all_offers << course.offers
-        else # e uma uc específica
-          all_offers << @curriculum_units.collect { |curriculum_unit| curriculum_unit.offers }
-        end
-
-      end
-
-      all_offers.flatten!
-      all_offers.uniq! # deixa apenas as ofertas únicas
-      
-      # de todas as ofertas encontradas, recupera apenas aquelas que o usuário tem acesso
-      @offers = all_offers.collect do |offer| 
-        access_course          = Allocation.have_access?(current_user.id, offer.course.allocation_tag.id)
-        access_curriculum_unit = Allocation.have_access?(current_user.id, offer.curriculum_unit.allocation_tag.id)
-        access_offer           = Allocation.have_access?(current_user.id, offer.allocation_tag.id)
-
-        # para adicionar a oferta à lista, o usuário deve ter acesso ao curso e à uc daquela oferta ou à própria oferta
-        offer if (access_course and access_curriculum_unit) or access_offer
-      end
-
-      @offers.compact! # remove os valores nulos
-
-    else
-
-      @offers = []
-
-    end
-
-  end
-
   ##
   # Método que recupera uma lista com as ucs e uma lista com os cursos que o usuário tem acesso partindo dos valores selecionados por ele
   # Um parâmetro de valor 0 corresponde à seleção de "Nenhum" no filtro, o que não é aceito para a pesquisa de ofertas
@@ -108,5 +50,27 @@ module OffersHelper
     return allocation_tags_ids.uniq
   end
 
+  ##
+  # Retorna o nome da uc e curso
+  ##
+  def get_uc_and_course_names(allocation_tags_ids)
+    # Como os valores que vêm das alocations são de ofertas, é necessário pegar apenas aquelas com uc e curso diferentes
+    allocation_tags_ids = allocation_tags_ids.uniq {|a| 
+      (AllocationTag.find(a).offer.course_id and AllocationTag.find(a).offer.curriculum_unit_id) 
+    }
+    
+    # Recupera os nomes de todas as ucs
+    ucs = allocation_tags_ids.collect {|a| 
+      CurriculumUnit.find(AllocationTag.find(a).offer.curriculum_unit_id).name
+    }.uniq
+
+    # Recupera os nomes de todas os cursos
+    courses = allocation_tags_ids.collect {|a| 
+      Course.find(AllocationTag.find(a).offer.course_id).name
+    }.uniq
+
+    return {"uc" => ucs.join(", "), "course" => courses.join(", ")}
+  end
 
 end
+
