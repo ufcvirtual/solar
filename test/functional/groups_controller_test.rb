8d0e9105e2debb20539048a92a959a946ea3c819
biancastephani <bianca.stephani@gmail.com>
2013-05-21 10:58:13 -0300

    [#49218133] Testes para remoção de Oferta e Turma, removendo, junto, o módulo default associado

--- test/functional/groups_controller_test.rb
+++ test/functional/groups_controller_test.rb
@@ -17,4 +17,28 @@ class GroupsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:groups)
   end
 
+  ##
+  # Destroy
+  ##
+
+  # Usuário com permissão e acesso (remove seu respectivo módulo default, pois não possui aulas)
+  test "remover turma" do 
+    assert_difference(["Group.count", "LessonModule.count"], -1) do 
+      get(:destroy, {:id => groups(:g9).id, allocation_tags_ids: [allocation_tags(:al22).id]})
+    end
+
+    assert_response :redirect
+    assert_equal flash[:notice], I18n.t(:successfully_deleted, :register => groups(:g9).code_semester)
+  end
+
+  # Usuário com permissão e acesso, mas a turma não permite (possui níveis inferiores)
+  test "nao remove turma - niveis inferiores" do
+    assert_no_difference(["Group.count", "LessonModule.count"]) do 
+      get :destroy, {:id => groups(:g2).id, allocation_tags_ids: [allocation_tags(:al2).id]}
+    end
+
+    assert_response :redirect
+    assert_equal flash[:alert], I18n.t(:cant_delete, :register => groups(:g2).code_semester)
+  end
+
 end
