24a96d9333ffabc65b1b6b80ff727a758e9374bd
biancastephani <bianca.stephani@gmail.com>
2013-05-23 16:59:13 -0300

    [#43020199] Correção na listagem de fóruns em forma de lista e alteração em alguns comentários

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,7 +2,8 @@ module PostsHelper
 
   def post_html(post, display_mode = 'list', can_interact = false)
     user     = post.user
-    children = Post.reorder_by_latest_posts(post.discussion_id, post.children) # reordenando os pots a partir dos seus "filhos/netos"
+    children = post.children
+    children = Post.reorder_by_latest_posts(post.discussion_id, children) unless (display_mode == 'list') # obtendo e reordenando os posts a partir dos seus "filhos/netos"
     editable = ((post.user_id == current_user.id) && (children.count == 0))
 
     child_html = ''
--- app/models/post.rb
+++ app/models/post.rb
@@ -59,8 +59,8 @@ class Post < ActiveRecord::Base
   def self.reorder_by_latest_posts(discussion_id, posts) 
     unless posts.empty? 
       # pega o primeiro post de cada "pai" de todos os posts, em ordem decrescente de updated_at, dos níveis inferiores ao atual, ou seja, pega os filhos/netos mais recentes
-      latest_posts      = Post.where("discussion_id = #{discussion_id} AND level > #{posts.first.level}").select("updated_at, parent_id, level").uniq_by{|a| a.parent_id}
-      lp_info = latest_posts.collect{|lp| lp.grandparent(posts.first.level)} # recuperar apenas a data e o id do "avô" dos posts mais recentes de cada nível
+      latest_posts = Post.where("discussion_id = #{discussion_id} AND level > #{posts.first.level}").select("updated_at, parent_id, level").uniq_by{|a| a.parent_id}
+      lp_info      = latest_posts.collect{|lp| lp.grandparent(posts.first.level)} # recuperar apenas a data e o id do "avô" dos posts mais recentes de cada nível
       # recuperar o mais recente dos agrupados, ou seja, se, para um mesmo "post pai", há vários níveis, em cada nível vai ter seu respectivo post mais atual. 
       # ao definir quais posts são "netos" do mesmo post do level inicial, pode-se saber qual, realmente, é o mais recente de todos e guardar sua data
       lp_info = lp_info.flatten.group_by{|lp| lp["grandparent_id"]}.collect{|lp| lp[1][0]}
@@ -69,7 +69,7 @@ class Post < ActiveRecord::Base
       posts = posts.sort{|post1, post2|
         lp_info1, lp_info2 = lp_info.find{|lp| lp["grandparent_id"] == post1.id}, lp_info.find{|lp| lp["grandparent_id"] == post2.id} # verifica se algum neto dos posts 1 e 2 estão na lista
         # se possuir neto mais recente, verifica se a data do neto é maior que a data do post. se for, a utiliza na comparação/ordenação; caso contrário, utiliza a própria data do post.
-        ((!lp_info2.nil? and lp_info2["date"] > post2.updated_at) ? lp_info2["date"] : post2.updated_at) <=> ((!lp_info1.nil? and lp_info1["date"] > post1.updated_at) ? lp_info1["date"] : post1.updated_at)
+        (((not lp_info2.nil?) and lp_info2["date"] > post2.updated_at) ? lp_info2["date"] : post2.updated_at) <=> (((not lp_info1.nil?) and lp_info1["date"] > post1.updated_at) ? lp_info1["date"] : post1.updated_at)
       }
     end
 
