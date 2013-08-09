ee2ee50f7c59fbfb4f1559a291adb305b9cfe1c4
joelmapxt <jlmpxt@gmail.com>
2013-08-09 18:02:04 -0300

    Conserto do filtro de matricula

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,23 +2,22 @@ class EnrollmentsController < ApplicationController
 
   def index
     authorize! :index, Enrollment
-
     @groups = []
-    @types  = CurriculumUnitType.order(:description)
+    @types  = CurriculumUnitType.order(:name)
     @student_profile = Profile.student_profile
-    @category_query  = params[:category_query]
+    @search_status  = params[:status]
+    @status = [["Todos", "all"], ["Matriculados", "enroll"]]
+    @curriculum_units = Enrollment.all_enrollments_by_user(current_user, @student_profile).map(&:offer).map(&:curriculum_unit)
 
     if current_user and (@student_profile != '')
       # recebe params[:offer] se foi pela pesquisa - MATRICULADOS e/ou ATIVOS
-      if params.include?(:offer)
-        @search_category = params[:offer][:category] if params[:offer].include?(:category)
-        @search_text     = params[:offer][:search] if params[:offer].include?(:search)
-      end
+        @search_category = params[:type] if params.include?(:type)
+        @search_curriculum_unit = params[:curriculum_unit] if params.include?(:curriculum_unit)
 
-      if @category_query == 'enroll' # lista apenas matriculados
-        @groups = Enrollment.enrollments_of_user(current_user, @student_profile, @search_category, @search_text)
+      if @search_status == 'enroll' # lista apenas matriculados
+        @groups = Enrollment.enrollments_of_user(current_user, @student_profile, @search_category, @search_curriculum_unit)
       else
-        @groups = Enrollment.all_enrollments_by_user(current_user, @student_profile, @search_category, @search_text)
+        @groups = Enrollment.all_enrollments_by_user(current_user, @student_profile, @search_category, @search_curriculum_unit)
       end
     end
   end
--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,24 +1,24 @@
 class Enrollment < ActiveRecord::Base
 
-  def self.enrollments_of_user(user, profile, offer_category = nil, curriculum_unit_name = nil)
+  def self.enrollments_of_user(user, profile, offer_category = nil, curriculum_unit_id = nil)
     query = []
     query << "curriculum_unit_type_id = #{offer_category}" unless offer_category.nil? or offer_category.empty?
-    query << "lower(name) ~ lower('#{curriculum_unit_name}')" unless curriculum_unit_name.nil? or curriculum_unit_name.empty?
+    query << "curriculum_units.id = #{curriculum_unit_id}" unless curriculum_unit_id.nil? or curriculum_unit_id.empty?
 
-    groups = Group.joins(offer: [:semester, curriculum_unit: :curriculum_unit_type]).where(query)
+    groups = Group.joins(offer: [:semester, curriculum_unit: :curriculum_unit_type]).where(query.join(" AND "))
     (groups & user.groups(profile)).uniq
   end
 
   ## - listar turmas para usuario pedir matricula
   ##   -- listar todas as turmas que podem receber matricula (olhar periodo de matricula na oferta)
   ##   -- quando matriculado em uma turma, as outras turmas dessa mesma oferta deixam de ser listadas
-  def self.all_enrollments_by_user(user, profile, offer_category = nil, curriculum_unit_name = nil)
+  def self.all_enrollments_by_user(user, profile, offer_category = nil, curriculum_unit_id = nil)
     query = []
     query << "curriculum_unit_type_id = #{offer_category}" unless offer_category.nil? or offer_category.empty?
-    query << "lower(name) ~ lower('#{curriculum_unit_name}')" unless curriculum_unit_name.nil? or curriculum_unit_name.empty?
+    query << "curriculum_units.id = #{curriculum_unit_id}" unless curriculum_unit_id.nil? or curriculum_unit_id.empty?
 
     # todas as turmas em que posso pedir matricula
-    can_enroll = Group.joins(offer: [:semester, curriculum_unit: :curriculum_unit_type]).where(curriculum_unit_types: {allows_enrollment: true}).where(query)
+    can_enroll = Group.joins(offer: [:semester, curriculum_unit: :curriculum_unit_type]).where(curriculum_unit_types: {allows_enrollment: true}).where(query.join(" AND "))
     can_enroll.select! { |group|
       (group.offer.enrollment_start_date.to_date..(group.offer.enrollment_end_date.try(:to_date) || group.offer.end_date.to_date)).include?(Date.today)
     }
