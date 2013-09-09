5641bfdbaa52e4885ad74f436786d52ebf668de6
Andrei Bosco <andreibosco@gmail.com>
2013-09-09 17:21:42 -0300

    [#56466598] ckeditor: movendo configuração da barra de ferramentas para as páginas que o utilizam (mensagem e forum)

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,15 +1,3 @@
-CKEDITOR.editorConfig = function( config )
-{
-    config.toolbar =
-    [
-        { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
-        { name: 'styles', items : [ 'Styles','Format' ] },
-        { name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
-        { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent', ] },
-        { name: 'links', items : [ 'Link','Unlink' ] }
-    ];
-}
-
 CKEDITOR.on( 'dialogDefinition', function( ev ) {
   var dialogName       = ev.data.name; // Recupera o nome da janela
   var dialogDefinition = ev.data.definition; // Recupera dados da janela
