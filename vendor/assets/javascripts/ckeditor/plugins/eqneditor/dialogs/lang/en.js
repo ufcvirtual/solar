871efbd9c61b7b1ae342aebf2a9958a973e4cd60
joelmap <jlmpxt@gmail.com>
2013-09-20 09:27:48 -0300

    [#57375012] implementação do editor matemático no forum

---
+++ vendor/assets/javascripts/ckeditor/plugins/eqneditor/dialogs/lang/en.js
@@ -0,0 +1,8 @@
+
+CKEDITOR.plugins.setLang( 'eqneditor',
+{
+	title		: 'Editor Matemático',
+	menu    : 'Maths',
+	toolbar	: 'Insert Equation',
+	edit		: 'Edit Equation'
+});
---
+++ vendor/assets/javascripts/ckeditor/plugins/eqneditor/lang/en.js
@@ -0,0 +1,8 @@
+
+CKEDITOR.plugins.setLang( 'eqneditor', 'en',
+{
+	title		: 'Editor Matemático',
+	menu    : 'Maths',
+	toolbar	: 'Insert Equation',
+	edit		: 'Edit Equation'
+});
