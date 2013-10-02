7c1ac9682112637822a335c6dc53006809e02dbd
biancastephani <bianca.stephani@gmail.com>
2013-10-02 10:57:20 -0300

    [#57993884] Adaptação dos testes funcionais e unitários de material de apoio para se adequar ao academic_allocation, novos testes unitários

--- test/functional/support_material_files_controller_test.rb
+++ test/functional/support_material_files_controller_test.rb
@@ -16,40 +16,41 @@ class SupportMaterialFilesControllerTest < ActionController::TestCase
   end
 
   test "listar material para edicao" do
-    get :list, {what_was_selected: %(false false false true), allocation_tags_ids: [allocation_tags(:al3).id]}
+    get :list, {allocation_tags_ids: [allocation_tags(:al3).id]}
     assert_response :success
-    assert_not_nil assigns(:allocation_tags)
+    assert_not_nil assigns(:allocation_tags_ids)
+    assert_not_nil assigns(:support_materials)
   end
 
   test "criar material do tipo link com protocolo default" do
-    assert_difference("SupportMaterialFile.count", 2) do
-      post(:create, {support_material_file: {url: "google.com"}, allocation_tags_ids: allocation_tags(:al3).id, material_type: Material_Type_Link}) #turma
-      post(:create, {support_material_file: {url: "google.com"}, allocation_tags_ids: allocation_tags(:al6).id, material_type: Material_Type_Link}) #oferta
+    assert_difference(["SupportMaterialFile.count", "AcademicAllocation.count"], 2) do
+      post(:create, {support_material_file: {url: "google.com", material_type: Material_Type_Link}, allocation_tags_ids: [allocation_tags(:al3).id]}) #turma
+      post(:create, {support_material_file: {url: "google.com", material_type: Material_Type_Link}, allocation_tags_ids: [allocation_tags(:al6).id]}) #oferta
     end
     assert_response :success
     assert_equal SupportMaterialFile.last.url, "http://google.com"
   end
 
   test "criar material do tipo arquivo" do
-    assert_difference('SupportMaterialFile.count', 1) do
-      post(:create, {support_material_file: {attachment: fixture_file_upload('files/file_10k.dat')}, allocation_tags_ids: allocation_tags(:al3).id, material_type: Material_Type_File})
+    assert_difference(["SupportMaterialFile.count", "AcademicAllocation.count"], 1) do
+      post(:create, {support_material_file: {attachment: fixture_file_upload('files/file_10k.dat'), material_type: Material_Type_File}, allocation_tags_ids: [allocation_tags(:al3).id]})
     end
     assert_response :success
   end
 
   test "nao criar novo material para uc ou curso" do
     # tentando criar para a UC de quimica 3 e o curso de licenciatura em quimica
-    assert_no_difference(["SupportMaterialFile.count"]) do
-      post(:create, {support_material_file: {url: "google.com"}, allocation_tags_ids: allocation_tags(:al13).id, material_type: Material_Type_Link})
-      post(:create, {support_material_file: {url: "google.com"}, allocation_tags_ids: allocation_tags(:al19).id, material_type: Material_Type_Link})
+    assert_no_difference(["SupportMaterialFile.count", "AcademicAllocation.count"]) do
+      post(:create, {support_material_file: {url: "google.com", material_type: Material_Type_Link}, allocation_tags_ids: [allocation_tags(:al13).id]})
+      post(:create, {support_material_file: {url: "google.com", material_type: Material_Type_Link}, allocation_tags_ids: [allocation_tags(:al19).id]})
     end
 
     assert_response :unprocessable_entity
   end
 
   test "download" do
-    assert_difference('SupportMaterialFile.count', 1) do
-      post(:create, {support_material_file: {attachment: fixture_file_upload('files/file_10k.dat')}, allocation_tags_ids: allocation_tags(:al3).id, material_type: Material_Type_File})
+    assert_difference(["SupportMaterialFile.count", "AcademicAllocation.count"], 1) do
+      post(:create, {support_material_file: {attachment: fixture_file_upload('files/file_10k.dat'), material_type: Material_Type_File}, allocation_tags_ids: [allocation_tags(:al3).id]})
     end
 
     get(:download, {id: SupportMaterialFile.last.id, allocation_tag_id: allocation_tags(:al3).id})
@@ -57,34 +58,34 @@ class SupportMaterialFilesControllerTest < ActionController::TestCase
   end
 
   test "editar" do
-    assert_difference("SupportMaterialFile.count", 1) do
-      post(:create, {support_material_file: {url: "google.com"}, allocation_tags_ids: allocation_tags(:al3).id, material_type: Material_Type_Link})
+    assert_difference(["SupportMaterialFile.count", "AcademicAllocation.count"], 1) do
+      post(:create, {support_material_file: {url: "google.com", material_type: Material_Type_Link}, allocation_tags_ids: [allocation_tags(:al3).id]})
     end
 
     last_material = SupportMaterialFile.last
     assert_equal last_material.url, "http://google.com"
 
-    assert_no_difference("SupportMaterialFile.count") do
-      put(:update, {id: last_material.id, support_material_file: {url: "youtube.com"}, allocation_tags_ids: allocation_tags(:al3).id})
+    assert_no_difference(["SupportMaterialFile.count", "AcademicAllocation.count"]) do
+      put(:update, {id: last_material.id, support_material_file: {url: "youtube.com"}, allocation_tags_ids: [allocation_tags(:al3).id]})
     end
 
     assert_equal "http://youtube.com", SupportMaterialFile.last.url
   end
 
   test "deletar" do
-    assert_difference("SupportMaterialFile.count", 1) do
-      post(:create, {support_material_file: {url: "google.com"}, allocation_tags_ids: allocation_tags(:al3).id, material_type: Material_Type_Link})
+    assert_difference(["SupportMaterialFile.count", "AcademicAllocation.count"], 1) do
+      post(:create, {support_material_file: {url: "google.com", material_type: Material_Type_Link}, allocation_tags_ids: [allocation_tags(:al3).id]})
     end
 
-    assert_difference("SupportMaterialFile.count", -1) do
+    assert_difference(["SupportMaterialFile.count", "AcademicAllocation.count"], -1) do
       delete(:destroy, {id: SupportMaterialFile.last.id, allocation_tags_ids: [allocation_tags(:al3).id]})
     end
   end
 
   test "deletar varios materiais" do
-    files = allocation_tags(:al3).support_material_files
-    assert_difference("SupportMaterialFile.count", -files.count) do
-      delete(:destroy, {id: files.map(&:id), allocation_tags_ids: [allocation_tags(:al3).id]})
+    materials = [support_material_files(:arquivo7), support_material_files(:url2), support_material_files(:url3)]
+    assert_difference(["SupportMaterialFile.count", "AcademicAllocation.count"], -materials.count) do
+      delete(:destroy, {id: materials.map(&:id), allocation_tags_ids: [allocation_tags(:al3).id]})
     end
   end
 
--- test/unit/support_material_file_test.rb
+++ test/unit/support_material_file_test.rb
@@ -3,43 +3,66 @@ require 'test_helper'
 class SupportMaterialFileTest < ActiveSupport::TestCase
 
   include ActionDispatch::TestProcess
-  fixtures :support_material_files
-
-  def setup
-    @at_quimica1 = allocation_tags(:al3).id
-  end
 
   test "criar material do tipo link com protocolo default" do
     assert_difference("SupportMaterialFile.count", 1) do 
-      SupportMaterialFile.create url: "google.com", allocation_tag_id: @at_quimica1, material_type: Material_Type_Link
+      SupportMaterialFile.create url: "google.com", material_type: Material_Type_Link
     end
 
     assert_equal SupportMaterialFile.last.url, "http://google.com"
   end
 
   test "criar material do tipo arquivo" do
-    material = SupportMaterialFile.new attachment: fixture_file_upload('files/file_10k.dat'), allocation_tag_id: @at_quimica1
+    material = SupportMaterialFile.new attachment: fixture_file_upload('files/file_10k.dat')
     assert_difference("SupportMaterialFile.count", 1) do 
       material.save
     end
+
+    assert material.valid?
   end
 
   test "nao criar material com tipo invalido" do
-    material = SupportMaterialFile.new attachment: fixture_file_upload('files/file_10k.exe'), allocation_tag_id: @at_quimica1
+    material = SupportMaterialFile.new attachment: fixture_file_upload('files/file_10k.exe')
     assert_no_difference("SupportMaterialFile.count") do 
       material.save
     end
     
+    assert material.invalid?
     assert material.errors.full_messages.join.strip.include?(I18n.t("activerecord.attributes.support_material_file.attachment_content_type").strip)
   end
 
   test "nao criar material do tipo arquivo por tamanho de arquivo" do
-    material = SupportMaterialFile.new attachment: fixture_file_upload('files/file_40m.dat'), allocation_tag_id: @at_quimica1
+    material = SupportMaterialFile.new attachment: fixture_file_upload('files/file_40m.dat')
     assert_no_difference("SupportMaterialFile.count") do 
       material.save
     end
 
+    assert material.invalid?
     assert_equal material.errors.full_messages.join.strip, I18n.t("activerecord.attributes.support_material_file.attachment_file_size").strip
+  end
+
+  test "material deve ter url valida se for de link" do
+    material = SupportMaterialFile.create(material_type: Material_Type_Link)
+
+    assert not(material.valid?)
+    assert_equal material.errors[:url].first, I18n.t(:blank, :scope => [:activerecord, :errors, :messages])
+
+    material = SupportMaterialFile.create(material_type: Material_Type_Link, url: "google")
+
+    assert not(material.valid?)
+    assert_equal I18n.t(:invalid, :scope => [:activerecord, :errors, :messages]), material.errors[:url].first
+  end
+
+  test "criando material completando a url com http quando necessario" do
+    material = SupportMaterialFile.create(material_type: Material_Type_Link, url: "www.google.com")
+    
+    assert material.valid?
+    assert_equal "http://www.google.com", material.url
+
+    material = SupportMaterialFile.create(material_type: Material_Type_Link, url: "https://www.google.com")
+    
+    assert material.valid?
+    assert_equal "https://www.google.com", material.url
   end
 
   test "editar" do
