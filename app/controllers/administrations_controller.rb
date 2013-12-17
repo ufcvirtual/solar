6bd1546b5545192cae63622da8a2ad402ecf8efe
biancastephani <bianca.stephani@gmail.com>
2013-12-17 16:50:19 -0300

    [#59245170] Na gerência de usuários da administração: Acréscimo das autorizações, descomentário dos testes funcionais existentes, acréscimos de testes para verificar autorizações, aviso ao mudar senha do usuário, acréscimo de notificações caso usuário não tenha permissão, correção do cancelar da edição de alocações

--- app/controllers/administrations_controller.rb
+++ app/controllers/administrations_controller.rb
@@ -1,67 +1,88 @@
 class AdministrationsController < ApplicationController
-
   layout false, except: [:manage_user, :manage_profiles]
-  #authorize_resource :class => false
-
 
   ## USERS
 
-
   def manage_user
+    authorize! :manage_user, Administration
     @types = Array.new(2,4)
     @types = [ [t(".name"), '0'], [t(".email"), '1'], [t(".username"), '2'], [t(".cpf"), '3'] ]
   end
 
   # Método chamado por ajax para buscar usuários
   def search_users
-  	type_search = params[:type_search]
-    @text_search = URI.unescape(params[:user]) unless params[:user].nil?
+    begin
+      authorize! :search_users, Administration
+      type_search = params[:type_search]
+      @text_search = URI.unescape(params[:user]) unless params[:user].nil?
 
-    unless @text_search.nil?
-      case type_search
-        when "0"
-          @users = User.where("lower(name) ~ '#{@text_search.downcase}'").paginate(page: params[:page] || 1, per_page: Rails.application.config.items_per_page)
-        when "1"
-          @users = User.where("lower(email) ~ '#{@text_search.downcase}'").paginate(page: params[:page] || 1, per_page: Rails.application.config.items_per_page)
-        when "2"
-          @users = User.where("lower(username) ~ '#{@text_search.downcase}'").paginate(page: params[:page] || 1, per_page: Rails.application.config.items_per_page)
-        else
-          @users = User.where("lower(cpf) ~ '#{@text_search.downcase}'").paginate(page: params[:page] || 1, per_page: Rails.application.config.items_per_page)
+      unless @text_search.nil?
+        case type_search
+          when "0"
+            @users = User.where("lower(name) ~ '#{@text_search.downcase}'").paginate(page: params[:page] || 1, per_page: Rails.application.config.items_per_page)
+          when "1"
+            @users = User.where("lower(email) ~ '#{@text_search.downcase}'").paginate(page: params[:page] || 1, per_page: Rails.application.config.items_per_page)
+          when "2"
+            @users = User.where("lower(username) ~ '#{@text_search.downcase}'").paginate(page: params[:page] || 1, per_page: Rails.application.config.items_per_page)
+          else
+            @users = User.where("lower(cpf) ~ '#{@text_search.downcase}'").paginate(page: params[:page] || 1, per_page: Rails.application.config.items_per_page)
+        end
       end
+    rescue CanCan::AccessDenied
+      render json: {success: false, alert: t(:no_permission)}, status: :unauthorized
     end
   end
 
   def show_user
-    @user = User.find(params[:id])
-    respond_to do |format|
-      format.html
-      format.json { render json: @user }
+    begin
+      authorize! :show_user, Administration
+      @user = User.find(params[:id])
+      respond_to do |format|
+        format.html
+        format.json { render json: @user }
+      end
+    rescue CanCan::AccessDenied
+      render json: {success: false, alert: t(:no_permission)}, status: :unauthorized
     end
   end
 
   def edit_user
-    @user = User.find(params[:id])
+    begin
+      authorize! :edit_user, Administration
+      @user = User.find(params[:id])
+    rescue CanCan::AccessDenied
+      render json: {success: false, alert: t(:no_permission)}, status: :unauthorized
+    end
   end
 
   def update_user
-    @user = User.find(params[:id])
-    active = (params[:status]=="1") ? true : false
-    @user.update_attributes(active: active, name: params[:name], email: params[:email])
-    respond_to do |format|
-      format.json { render json: {status: "ok"}  }
+    begin 
+      authorize! :update_user, Administration
+      @user = User.find(params[:id])
+      active = (params[:status]=="1") ? true : false
+      @user.update_attributes(active: active, name: params[:name], email: params[:email])
+      render json: {status: "ok"}
+    rescue CanCan::AccessDenied
+      render json: {success: false, alert: t(:no_permission)}, status: :unauthorized
     end
   end
 
   def change_password
     begin
-      @user = User.find(params[:id]).send_reset_password_instructions
-      respond_to do |format|
-        format.json { render json: {status: "ok"}  }
+      authorize! :change_password, Administration
+      @user = User.find(params[:id])
+
+      Thread.new do
+        Mutex.new.synchronize do
+          @user.send_reset_password_instructions
+        end
       end
+
+      render json: {status: "ok", notice: t("administrations.success.email_sent")}
+    rescue CanCan::AccessDenied
+      render json: {success: false, alert: t(:no_permission)}, status: :unauthorized
     rescue
-      respond_to do |format|
-        format.json { render json: {success: false}  }
-      end 
+      render json: {success: false, alert: t("administrations.success.email_not_sent")}
     end
   end
 
@@ -70,36 +91,55 @@ class AdministrationsController < ApplicationController
 
 
   def allocations_user
-    id = params[:id]
-    @allocations_user = User.find(id).allocations
-                          .joins("LEFT JOIN allocation_tags at ON at.id = allocations.allocation_tag_id")
-                          .where("(at.curriculum_unit_id is not null or at.offer_id is not null or at.group_id is not null )")
+    begin
+      authorize! :allocations_user, Administration
+      @allocations_user = User.find(params[:id]).allocations
+                            .joins("LEFT JOIN allocation_tags at ON at.id = allocations.allocation_tag_id")
+                            .where("(at.curriculum_unit_id is not null or at.offer_id is not null or at.group_id is not null )")
 
-    @profiles = @allocations_user.map(&:profile).flatten.uniq
+      @profiles = @allocations_user.map(&:profile).flatten.uniq
 
-    @periods = [ [t(:active),''] ]
-    @periods += Semester.all().map{|s| s.name}.flatten.uniq.sort! {|x,y| y <=> x}
+      @periods = [ [t(:active),''] ]
+      @periods += Semester.all.map{|s| s.name}.flatten.uniq.sort! {|x,y| y <=> x}
+    rescue CanCan::AccessDenied
+      render json: {success: false, alert: t(:no_permission)}, status: :unauthorized
+    end
   end
 
   def show_allocation
-    @allocation = Allocation.find(params[:id])
-    respond_to do |format|
-      format.html
-      format.json { render json: @allocation}
+    begin
+      authorize! :show_allocation, Administration
+      @allocation = Allocation.find(params[:id])
+      respond_to do |format|
+        format.html
+        format.json { render json: @allocation}
+      end
+    rescue CanCan::AccessDenied
+      render json: {success: false, alert: t(:no_permission)}, status: :unauthorized
     end
   end
 
   def edit_allocation
-    @allocation = Allocation.find(params[:id])
+    begin
+      authorize! :edit_allocation, Administration
+      @allocation = Allocation.find(params[:id])
+    rescue CanCan::AccessDenied
+      render json: {success: false, alert: t(:no_permission)}, status: :unauthorized
+    end
   end
 
   def update_allocation
-    @allocation = Allocation.find(params[:id])
-    @allocation.update_attribute(:status, params[:status])
+     begin 
+      authorize! :update_allocation, Administration
+      @allocation = Allocation.find(params[:id])
+      @allocation.update_attribute(:status, params[:status])
 
-    respond_to do |format|
-      format.html { render action: :show_allocation, id: params[:id] }
-      format.json { render json: {status: "ok"}  }
+      respond_to do |format|
+        format.html { render action: :show_allocation, id: params[:id] }
+        format.json { render json: {status: "ok"}  }
+      end
+    rescue CanCan::AccessDenied
+      render json: {success: false, alert: t(:no_permission)}, status: :unauthorized
     end
   end
 
--- test/functional/administrations_controller_test.rb
+++ test/functional/administrations_controller_test.rb
@@ -5,8 +5,10 @@ class AdministrationsControllerTest < ActionController::TestCase
   include Devise::TestHelpers
 
   def setup
+    @admin  = users(:admin)
     @editor = users(:editor)
-    sign_in @editor
+    @aluno1 = users(:aluno1)
+    sign_in @admin
   end
 
   test "acessar pagina de administracao de usuario" do
@@ -14,33 +16,51 @@ class AdministrationsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  # test "nao acessar administracao de usuario sem permissao" do 
-  #   sign_out 
-  #   sign_in users(:aluno1)
-  #   get :manage_user
-  #   assert_response :redirect
-  #   assert_redirected_to home_path
-  #   assert_equal flash[:alert], I18n.t(:no_permission)
-  # end
+  test "nao acessar administracao de usuario sem permissao" do 
+    sign_in @editor
+    get :manage_user
+    assert_response :redirect
+    assert_redirected_to home_path
+    assert_equal flash[:alert], I18n.t(:no_permission)
+  end
 
   test "buscar usuario" do
-    get :search_users, :user => 'aluno 1', :type_search => 0
+    get :search_users, user: 'aluno 1', type_search: 0
     assert_not_nil assigns(:users)
     assert_equal users(:aluno1).name, assigns(:users).first.name
   end
 
+  test "nao buscar usuario sem permissao" do 
+    sign_in @editor
+    get :search_users, user: 'aluno 1', type_search: 0
+    assert_nil assigns(:users)
+    assert_response :unauthorized
+    assert_equal get_json_response("alert"), I18n.t(:no_permission)
+  end
+
   test "buscar usuario nao retorna dados" do
-    get :search_users, :user => 'aluno xyz', :type_search => 0
+    get :search_users, user: 'aluno xyz', type_search: 0
     assert assigns(:users).empty?
   end
 
   test "editar usuario" do
-    user = users(:aluno1)
     assert_no_difference(["User.count"]) do
-      put :update_user, { id: user.id, name: "aluno1 alterado", email: 'email_aluno1@qq.com'}
+      put :update_user, { id: @aluno1.id, name: "aluno1 alterado", email: 'email_aluno1@qq.com'}
     end
 
-    assert_equal User.find(user.id).name, "aluno1 alterado"
+    assert_equal "aluno1 alterado", User.find(@aluno1.id).name
+  end
+
+  test "nao editar usuario sem permissao" do 
+    sign_in @editor
+    
+    assert_no_difference(["User.count"]) do
+      put :update_user, { id: @aluno1.id, name: "aluno1 alterado", email: 'email_aluno1@qq.com'}
+    end
+    assert_not_equal "aluno1 alterado", User.find(@aluno1.id).name
+
+    assert_response :unauthorized
+    assert_equal get_json_response("alert"), I18n.t(:no_permission)
   end
 
   test "editar status de alocacao de usuario" do
@@ -50,6 +70,20 @@ class AdministrationsControllerTest < ActionController::TestCase
     end
 
     assert_equal Allocation.find(allocation.id).status, Allocation_Cancelled
+  end
+
+  test "nao editar status de alocacao sem permissao" do 
+    sign_in @editor
+    
+    allocation = allocations(:aluno3_al8)
+    assert_no_difference(["Allocation.count"]) do
+      put :update_allocation, { id: allocation.id, status: Allocation_Cancelled}
+    end
+
+    assert_not_equal Allocation.find(allocation.id).status, Allocation_Cancelled
+
+    assert_response :unauthorized
+    assert_equal get_json_response("alert"), I18n.t(:no_permission)
   end
 
 end
