49bf0b955700dd02bebb966d9862083022b5fab6
Wedson Lima <wedson.sousa.lima@gmail.com>
2014-01-23 16:39:47 -0300

    Lista apps somente se o usuario estiver logado. Precisa melhorar.

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -12,11 +12,20 @@ Doorkeeper.configure do
   end
 
   # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.
-  # admin_authenticator do
-  #   # Put your admin authentication logic here.
-  #   # Example implementation:
-  #   Admin.find_by_id(session[:admin_id]) || redirect_to(new_admin_session_url)
-  # end
+  admin_authenticator do
+    # Put your admin authentication logic here.
+    # Example implementation:
+
+    if current_user
+      # can?(:index, OAuthApplication) ? current_user : redirect_to(home_path, alert: t(:no_permission))
+      current_user
+    else
+      redirect_to(login_url)
+    end
+
+    # (can?(:manage, :doorkeeper) ? current_user : redirect_to(pages_path)) || redirect_to(login_url)
+    #Admin.find_by_id(session[:admin_id]) || redirect_to(new_admin_session_url)
+  end
 
   # Authorization Code expiration time (default 10 minutes).
   # authorization_code_expires_in 10.minutes
