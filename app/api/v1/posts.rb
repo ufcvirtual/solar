7f3ab7a97da28bc62adb5eacb6ec34e2de20bc71
Bruno Neves <bruno@virtual.ufc.br>
2014-01-30 17:01:09 -0300

    [#64656642] Chamada de deleção de post e de arquivo de um post

--- app/api/v1/posts.rb
+++ app/api/v1/posts.rb
@@ -1,7 +1,7 @@
 module V1
   class Posts < Base
 
-    # guard_all!
+    guard_all!
 
     namespace :discussions do
       get ":id/posts", rabl: "posts/list" do
@@ -9,5 +9,37 @@ module V1
       end
     end
 
+    namespace :posts do
+      desc "Delete a post."
+      params do
+        requires :id, type: Integer, desc: "Post ID."
+      end
+      delete ':id' do
+        begin
+          current_user.discussion_posts.find(params[:id]).destroy
+        rescue
+          error!({}, 401)
+        end  
+      end
+
+      desc "Delete a file of post."
+      params do
+        requires :id, type: Integer, desc: "File Post ID."
+      end
+
+      delete 'files/:id' do
+        begin
+          if current_user.discussion_post_ids.include?(PostFile.find(params[:id]).discussion_post_id) 
+            PostFile.find(params[:id]).destroy 
+          else
+            raise  
+          end 
+        rescue 
+          error!({}, 401)
+        end  
+      end
+
+    end
+  
   end
 end
