982b26fbf8099f768cca3ab70cf579dcc8c5a91f
Wedson Lima <wedson.sousa.lima@gmail.com>
2014-02-11 15:02:15 -0300

    [#62257852] Webserver: carga de turmas

--- app/api/v1/groups.rb
+++ app/api/v1/groups.rb
@@ -1,14 +1,112 @@
 module V1
   class Groups < Base
-    guard_all!
-
     namespace :curriculum_units do
       desc "Turmas da UC"
       params do
         requires :id, type: Integer
       end
       get ":id/groups", rabl: "groups/list" do
+        guard!
         @groups = CurriculumUnit.where(id: params[:id]).first.try(:groups) || []
+      end
+    end
+
+    ## webserver
+
+    helpers do
+      def verify_or_create_semester(name, offer_period)
+        semester = Semester.where(name: name).first_or_initialize
+
+        if semester.new_record?
+          semester.build_offer_schedule offer_period
+          semester.build_enrollment_schedule start_date: offer_period[:start_date], end_date: offer_period[:start_date] # one day for enrollment
+          semester.save!
+        end
+
+        semester
+      end
+
+      def verify_or_create_offer(semester, course, uc, offer_period)
+        offer = Offer.where(semester_id: semester, course_id: course, curriculum_unit_id: uc).first_or_initialize
+
+        if offer.new_record?
+          ss = semester.offer_schedule
+          offer.build_period_schedule(offer_period) if ss.start_date != offer_period[:start_date] or ss.end_date != offer_period[:end_date] # semester offer period != offer period
+          offer.save!
+        else
+          # o que fazer quando as datas de oferta e semestre são diferentes?
+            ## cadastra as turmas sem mudar a data da oferta? cria uma nova oferta(nao faz muito sentido)? não faz nada e retorna um erro?
+        end
+
+        offer
+      end
+
+      def verify_or_create_group(offer, code)
+        group = Group.where(code: code, offer_id: offer).first_or_initialize
+        group.status = true
+        group.save!
+        group
+      end
+
+      def allocate_professors(group, cpfs)
+        ## como vai ficar? como saber quem eh professor?
+        ## Prof. Titular => 2
+
+        professors = User.where(cpf: cpfs)
+
+        professors.each do |prof|
+          group.allocate_user(prof.id, 2)
+        end
+      end
+
+    end
+
+    ## duvidas ##
+
+    # ActiveRecord::RecordInvalid
+    # Offer schedule start date não deve preceder o ano atual, Offer schedule end date não deve preceder o dia de hoje, Enrollment schedule start date não deve preceder o ano atual, Enrollment schedule end date não deve preceder o dia de hoje
+
+    # <load_turma>
+    #   <ano>2014</ano>
+    #   <periodo>2</periodo>
+    #   <cod_disciplina>RM404</cod_disciplina>
+    #   <cod_turma>RM0121</cod_turma>
+    #   <cod_curso>RM404</cod_curso> <!-- graduacao -->
+    #   <data_inicio>15/02/2014</data_inicio>
+    #   <data_fim>01/12/2014</data_fim>
+    #   <professores type="array">
+    #     <value>565656565</value>
+    #     <value>565656566</value>
+    #     <value>600626565</value>
+    #   </professores>
+    # </load_turma>
+
+    ## modulo academico :: carga de turmas
+    namespace :load do
+      format :xml
+      post :groups do
+        # valids IPs
+        raise ActiveRecord::RecordNotFound unless YAML::load(File.open('config/webserver.yml'))[Rails.env.to_s]['address'].include?(request.env['REMOTE_ADDR'])
+
+        load_group    = params[:load_turma]
+        cpfs          = load_group[:professores]
+        semester_name = "#{load_group[:ano]}.#{load_group[:periodo]}"
+        offer_period  = { start_date: load_group[:data_inicio].to_date, end_date: load_group[:data_fim].to_date }
+        group_code    = load_group[:cod_turma]
+        course        = Course.find_by_code load_group[:cod_curso]
+        uc            = CurriculumUnit.find_by_code load_group[:cod_disciplina]
+
+        begin
+          semester = verify_or_create_semester(semester_name, offer_period)
+          offer    = verify_or_create_offer(semester, course, uc, offer_period)
+          group    = verify_or_create_group(offer, group_code)
+
+          allocate_professors(group, cpfs)
+
+          {ok: :ok}
+        rescue => error
+          error!({error: error}, 422)
+        end
       end
     end
 
