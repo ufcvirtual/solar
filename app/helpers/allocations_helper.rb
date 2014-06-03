d164f1a9065d646e321aa93d6dbc11dc1d017225
Bianca <bianca.stephani@gmail.com>
2014-06-03 13:33:41 -0300

    [#master] Correção de passagem de parâmetro na busca de usuários em alocação

--- app/helpers/allocations_helper.rb
+++ app/helpers/allocations_helper.rb
@@ -15,7 +15,7 @@ module AllocationsHelper
   ## Rtorna os perfis disponíveis para alocação de determinado usuário em uma lista de allocations_tags
   def profiles_available_for_allocation(user_id, allocation_tags_ids)
     profiles_allocated = Profile.all(:joins => [:allocations], 
-      :conditions => ["allocation_tag_id IN (#{allocation_tags_ids}) AND user_id = (#{user_id})"])
+      :conditions => ["allocation_tag_id IN (#{allocation_tags_ids.gsub(/ /, ',')}) AND user_id = (#{user_id})"])
 
     query = profiles_allocated.empty? ? '' : " AND id NOT IN (#{profiles_allocated.map(&:id).join(',')})"
 
