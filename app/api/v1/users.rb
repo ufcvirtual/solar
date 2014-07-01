ddbee2e23c49c3db515a74ea366e106821adfce6
Wedson Lima <wedson.sousa.lima@gmail.com>
2014-07-01 15:10:04 -0300

    [#74187766] API mobilis: fotos dos usuarios

--- app/api/v1/users.rb
+++ app/api/v1/users.rb
@@ -4,8 +4,27 @@ module V1
 
     namespace :users
 
+    # GET /users/me
     get :me, rabl: "users/show" do
       @user = current_user
     end
+
+    # GET /users/1/photo
+    get "/:id/photo" do
+      begin
+        user = User.find(params[:id])
+        photo = user.photo.path(params[:style] || :small)
+        filename = Digest::MD5.hexdigest(user.cpf)
+
+        content_type MIME::Types.type_for(filename)[0].to_s
+        env['api.format'] = :binary
+        header "Content-Disposition", "attachment; filename*=UTF-8''#{URI.escape(filename)}"
+
+        File.open(photo).read
+      rescue
+        raise ActiveRecord::RecordNotFound
+      end
+    end
+
   end
 end
