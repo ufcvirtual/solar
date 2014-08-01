065ae343a53641719edd645509a333ce1963eae5
Bianca <bianca.stephani@gmail.com>
2014-08-01 14:46:31 -0300

    [#76048702] Alteração da mensagem do 'esqueci senha' quando o usuário for integrado; acréscimo de mensagem temporária à tela de login

--- app/controllers/devise/users_passwords_controller.rb
+++ app/controllers/devise/users_passwords_controller.rb
@@ -1,3 +1,12 @@
 class Devise::UsersPasswordsController < Devise::PasswordsController
   include SysLog::Devise
+
+  def create
+    user = User.find_by_email(params[:user][:email])
+    if not(user.nil?) and user.integrated?
+      redirect_to login_path, notice: t("devise.users_passowrds.ma_request").html_safe
+    else
+      super
+    end
+  end
 end
