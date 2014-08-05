5e878305ae2903f442b8e1c80867f8a51a29b615
Wedson Lima <wedson.sousa.lima@gmail.com>
2014-08-05 15:04:02 -0300

    [#76245420] Atualizando migrate q deu erro em producao / update fixtures

--- db/migrate/20140804143850_add_academic_to_chat_messages.rb
+++ db/migrate/20140804143850_add_academic_to_chat_messages.rb
@@ -16,25 +16,27 @@ class AddAcademicToChatMessages < ActiveRecord::Migration
     #   end
     # end
 
+
+    change_column :chat_messages, :chat_room_id, :integer, null: true
     # remove_column :chat_messages, :chat_room_id
   end
 
   def down
-    change_table :chat_messages do |t|
-      t.references :chat_room
-      t.index :chat_room_id
-    end
+    # change_table :chat_messages do |t|
+    #   t.references :chat_room
+    #   t.index :chat_room_id
+    # end
 
-    ChatMessage.find_each do |msg|
-      exist_copy_from_this_msg = ChatMessage.where(msg.attributes.except('id', 'academic_allocation_id', 'created_at').merge('chat_room_id' => msg.academic_allocation.academic_tool_id)).size > 0
-      if exist_copy_from_this_msg
-        msg.destroy
-      else
-        msg.update_attribute(:chat_room_id, msg.academic_allocation.academic_tool_id)
-      end
+    # ChatMessage.find_each do |msg|
+    #   exist_copy_from_this_msg = ChatMessage.where(msg.attributes.except('id', 'academic_allocation_id', 'created_at').merge('chat_room_id' => msg.academic_allocation.academic_tool_id)).size > 0
+    #   if exist_copy_from_this_msg
+    #     msg.destroy
+    #   else
+    #     msg.update_attribute(:chat_room_id, msg.academic_allocation.academic_tool_id)
+    #   end
+    # end
 
-    end
-
+    change_column :chat_messages, :chat_room_id, :integer, null: false
     remove_column :chat_messages, :academic_allocation_id
   end
 end
