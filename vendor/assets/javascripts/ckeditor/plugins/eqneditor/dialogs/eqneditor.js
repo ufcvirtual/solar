f6be8f1695eb19813fa406a922d98d6e14f6dae7
Joelmap <jlmpxt@gmail.com>
2014-09-03 10:40:52 -0300

    [#77979912] Correção da tradução do ckeditor

--- vendor/assets/javascripts/ckeditor/plugins/eqneditor/dialogs/eqneditor.js
+++ vendor/assets/javascripts/ckeditor/plugins/eqneditor/dialogs/eqneditor.js
@@ -12,7 +12,7 @@ CKEDITOR.dialog.add( 'eqneditorDialog', function(editor)
 {	
 	var http = ('https:' == document.location.protocol ? 'https://' : 'http://');
 	return {
-		title : editor.lang.eqneditor,
+		title : editor.lang.eqneditor.title,
 		minWidth : 567,
 		minHeight : 430,
 		resizable: CKEDITOR.DIALOG_RESIZE_NONE,
