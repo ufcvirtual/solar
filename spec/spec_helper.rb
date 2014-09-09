c2a7145d4ea51bab23f3a9835f1d57b3f9053944
Bianca <bianca.stephani@gmail.com>
2014-09-09 11:52:15 -0300

    [#master] Alterando de volta configuração do rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,7 @@ Spork.prefork do
   # need to restart spork for it take effect.
 
   # This file is copied to spec/ when you run 'rails generate rspec:install'
-  ENV["RAILS_ENV"] = 'development'
+  ENV["RAILS_ENV"] = 'test'
 
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
