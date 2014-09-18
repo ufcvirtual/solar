6939fc0e44cc4ce10ba688097760bbbcb5f2e90a
Wedson Lima <wedson.sousa.lima@gmail.com>
2014-09-18 16:33:24 -0300

    [#78856238] Aulas / teste de modulo

--- test/functional/lesson_modules_controller_test.rb
+++ test/functional/lesson_modules_controller_test.rb
@@ -12,35 +12,38 @@ class LessonModulesControllerTest < ActionController::TestCase
 
   # New/Create
 
-  test 'cria modulo' do 
+  test 'cria modulo' do
     sign_in @editor
-    get :new, {:allocation_tags_ids => "#{allocation_tags(:al3).id}"} 
+    get :new, {allocation_tags_ids: "#{allocation_tags(:al3).id}"}
     assert_not_nil assigns(:allocation_tags_ids)
     assert_not_nil assigns(:lesson_module)
 
-    assert_difference("LessonModule.count", 1) do 
-      post(:create, {:lesson_module => {:name => "Modulo 01"}, :allocation_tags_ids => "#{assigns(:allocation_tags_ids)}"})
+    assert_difference("LessonModule.count", 1) do
+      post(:create, {lesson_module: {name: "Modulo 01", academic_allocation_attributes: {'0'=> {allocation_tag_id: allocation_tags(:al3).id}}}, allocation_tags_ids: "#{allocation_tags(:al3).id}"})
     end
 
     assert_response :success
   end
 
-  test 'nao cria modulo - sem permissao' do 
+  test 'nao cria modulo - sem permissao' do
     sign_in users(:professor)
-    get :new, {:allocation_tags_ids => "#{allocation_tags(:al2).id}"}
+    get :new, {allocation_tags_ids: "#{allocation_tags(:al2).id}"}
+
+    assert_response :redirect
+
     assert_not_nil assigns(:allocation_tags_ids)
     assert_nil assigns(:lesson_module)
 
-    assert_no_difference("LessonModule.count") do 
-      post(:create, {:lesson_module => {:name => "Modulo 01"}, :allocation_tags_ids => "#{assigns(:allocation_tags_ids)}"})
+    assert_no_difference("LessonModule.count") do
+      post(:create, {lesson_module: {name: "Modulo 01", academic_allocation_attributes: {'0'=> {allocation_tag_id: allocation_tags(:al3).id}}}, allocation_tags_ids: "#{allocation_tags(:al3).id}"})
     end
 
-    assert_response :error
+    assert_response :redirect
   end
 
   # Edit/Update
 
-  test 'edita modulo' do 
+  test 'edita modulo' do
     get :edit, {:id => lesson_modules(:module2).id, :allocation_tags_ids => "#{allocation_tags(:al2).id}"}
     assert_not_nil assigns(:lesson_module)
 
@@ -50,7 +53,7 @@ class LessonModulesControllerTest < ActionController::TestCase
     assert_equal "Modulo 01", assigns(:lesson_module).name
   end
 
-  test 'nao edita modulo - sem permissao' do 
+  test 'nao edita modulo - sem permissao' do
     sign_in @professor
     get :edit, {:id => lesson_modules(:module2).id, :allocation_tags_ids => "#{allocation_tags(:al2).id}"}
 
@@ -59,23 +62,22 @@ class LessonModulesControllerTest < ActionController::TestCase
 
     put(:update, {:id => lesson_modules(:module2).id, :lesson_module => {:name => "Modulo 01"}, :allocation_tags_ids => "#{allocation_tags(:al2).id}"})
 
-    assert_response :error
-    assert_not_equal "Modulo 01", assigns(:lesson_module).name
+    assert_response :redirect
   end
 
   # Destroy
 
-  test 'deleta modulo - com aulas' do 
+  test 'deleta modulo - com aulas' do
     assert_difference("LessonModule.count", -1) do
       assert_no_difference("Lesson.count") do
-        get :destroy, {:id => lesson_modules(:module3).id, :allocation_tags_ids => "#{allocation_tags(:al2).id}"} 
+        get :destroy, {:id => lesson_modules(:module3).id, :allocation_tags_ids => "#{allocation_tags(:al2).id}"}
       end
     end
 
     assert_response :success
   end
 
-  test 'nao deleta modulo - sem permissao' do 
+  test 'nao deleta modulo - sem permissao' do
     sign_in @professor
 
     assert_no_difference(["LessonModule.count", "Lesson.count"]) do
