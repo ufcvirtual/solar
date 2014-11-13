7f9bea194ed1467a802198cb7c4f9ef31202153f
Bianca <bianca.stephani@gmail.com>
2014-11-13 14:06:40 -0300

    [#80333918] Integração com SAV - Acréscimo de atributos no retorno da lista de turmas

--- app/api/templates/groups/group.rabl
+++ app/api/templates/groups/group.rabl
@@ -3,7 +3,7 @@ object @group
 attributes :id, :code, :offer_id
 
 glue @group.offer do
-  attributes :start_date, :end_date
+  attributes :start_date, :end_date, :course_id, :curriculum_unit_id
 end
 
 node :students do |group|
--- app/helpers/sav_helper.rb
+++ app/helpers/sav_helper.rb
@@ -10,13 +10,14 @@ module SavHelper
         cipher.encrypt
         cipher.iv, cipher.key  = SavConfig::IV, SavConfig::KEY
 
-        user_profiles = current_user.profiles.where("(allocations.allocation_tag_id IN (?) OR allocations.allocation_tag_id IS NULL)", allocation_tags_ids).pluck(:id)
+        user_profiles = current_user.profiles.where("(allocations.allocation_tag_id IN (?))", allocation_tags_ids).pluck(:id)
         savs = Sav.current_savs(allocation_tags_ids).where("profile_id IN (?) OR profile_id IS NULL", user_profiles).pluck(:id).to_s.delete("[]")
 
         @_sav_url = (savs.empty? ? "" : [SavConfig::URL, Base64.encode64(cipher.update(
           SavConfig::PARAMS.gsub("user_cpf", current_user.cpf).gsub("user_name", current_user.name).gsub("profiles_ids", user_profiles.to_s.delete("[]"))
           .gsub("questionnaires_ids", savs).gsub("taggable_id", at.send(at.refer_to).try(:id).to_s)
         ) + cipher.final).gsub("\n",'')].join.html_safe)
+        # user_session[:tabs][:opened][user_session[:tabs][:active]].merge!({sav_url: @_sav_url})
       end
     end
 
