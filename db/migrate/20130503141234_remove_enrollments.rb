49cb6836b5a70a98e414966d6bba719921271fd0
Bianca <bianca.stephani@gmail.com>
2014-11-18 11:35:34 -0300

    [#82841550] Corrigindo problema ao rodar migrates

--- db/migrate/20130503141234_remove_enrollments.rb
+++ db/migrate/20130503141234_remove_enrollments.rb
@@ -1,22 +1,23 @@
 class RemoveEnrollments < ActiveRecord::Migration
   def up
     # Passa os valores salvos em enrollments para ofertas/schedules
-    Enrollment.all.each do |enroll|
-      offer = Offer.find(enroll.offer_id)
-      if offer.schedule.nil? 
-        schedule = Schedule.create(start_date: enroll.start, end_date: enroll.end)
-      else
-        schedule = offer.schedule
-        schedule.update_attributes(start_date: enroll.start, end_date: enroll.end)
-      end
-      offer.update_attribute(:schedule_id, schedule.id)
-    end
+    
+    # Enrollment.all.each do |enroll|
+    #   offer = Offer.find(enroll.offer_id)
+    #   if offer.schedule.nil? 
+    #     schedule = Schedule.create(start_date: enroll.start, end_date: enroll.end)
+    #   else
+    #     schedule = offer.schedule
+    #     schedule.update_attributes(start_date: enroll.start, end_date: enroll.end)
+    #   end
+    #   offer.update_attribute(:schedule_id, schedule.id)
+    # end
 
-    # Preenche as offers sem schedule, estas recebem como data inicial do período de matrícula sua própria data inicial (isto é feito apenas para não ficar vazio)
-    Offer.find_all_by_schedule_id(nil).each do |offer|
-      schedule = Schedule.create(start_date: offer.start_date)
-      offer.update_attribute(:schedule_id, schedule.id)
-    end
+    # # Preenche as offers sem schedule, estas recebem como data inicial do período de matrícula sua própria data inicial (isto é feito apenas para não ficar vazio)
+    # Offer.find_all_by_schedule_id(nil).each do |offer|
+    #   schedule = Schedule.create(start_date: offer.start_date)
+    #   offer.update_attribute(:schedule_id, schedule.id)
+    # end
 
     # Remove a tabela de enrollments
     drop_table :enrollments
