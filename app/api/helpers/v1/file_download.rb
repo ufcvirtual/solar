ae57e873b3f75aa205b496775d9fa77ff53a1b46
Wedson Lima <wedson.sousa.lima@gmail.com>
2014-12-16 12:28:53 -0300

    [#84039442] api / acesso as aulas

--- app/api/helpers/v1/file_download.rb
+++ app/api/helpers/v1/file_download.rb
@@ -1,11 +1,13 @@
 module V1::FileDownload
 
-  def send_file(filepath, filename)
+  def send_file(filepath, filename, disposition = 'inline')
     raise ActiveRecord::RecordNotFound unless File.exist?(filepath)
 
-    content_type MIME::Types.type_for(filepath)[0].to_s
+    # disposition = filename.index(/html?/) ? 'inline' : 'attachment'
+
     env['api.format'] = :binary
-    header "Content-Disposition", "attachment; filename*=UTF-8''#{Digest::MD5.hexdigest(URI.escape(filename))}"
+    content_type MIME::Types.type_for(filepath)[0].to_s
+    header "Content-Disposition", "#{disposition}; filename*=UTF-8''#{Digest::MD5.hexdigest(URI.escape(filename))}"
 
     File.open(filepath).read
   end
--- app/api/v1/lessons.rb
+++ app/api/v1/lessons.rb
@@ -1,12 +1,26 @@
 module V1
   class Lessons < Base
 
+    namespace :groups do
+      before { guard! }
+
+      ## api/v1/groups/1/lessons
+      desc "Lista de aulas da turma"
+      params { requires :id, type: Integer }
+      get ":id/lessons", rabl: "lessons/list" do
+        @ats = RelatedTaggable.related(group_id: params[:id])
+        @lessons_modules = LessonModule.to_select(@ats, current_user, list = true)
+      end
+
+    end # namespace groups
+
     namespace :lessons do
       ## aulas publicas
-      before { verify_ip_access! }
 
       ## api/v1/lessons
       get "/" do
+        verify_ip_access!
+
         if params[:disconsider].present?
           disconsider = params[:disconsider]
           query_not = 'id > ?'
@@ -24,35 +38,20 @@ module V1
         end
         courses
       end # get /
-    end
 
-    segment do
-      before { guard! }
+      ## api/v1/lessons/1/aula1/index.thml
+      desc "Acessa aula"
+      get ":id(/:folder)/*path" do
+        lesson = Lesson.find(params[:id])
 
-      namespace :groups do
+        # guarda acesso apenas para o arquivo inicial
+        guard! if lesson.address == (relative_path = File.join([params[:folder], params[:path]].compact))
+        file_path = Lesson::FILES_PATH.join(params[:id], relative_path)
 
-        ## api/v1/groups/1/lessons
-        desc "Lista de aulas da turma"
-        params { requires :id, type: Integer }
-        get ":id/lessons", rabl: "lessons/list" do
-          @ats = RelatedTaggable.related(group_id: params[:id])
-          @lessons_modules = LessonModule.to_select(@ats, current_user, list = true)
-        end
+        send_file(file_path.to_s, file_path.to_s)
+      end # get folder
 
-      end # namespace
-
-      namespace :lessons do
-
-        desc "Download de aulas"
-        params { requires :id, type: Integer, desc: "ID da aula" }
-        get ":id/download" do
-          file = Lesson.find(params[:id])
-          send_file(file.path(fullpath=true).to_s, file.address.to_s)
-        end
-
-      end # namespace
-
-    end # segment
+    end # namespace lessons
 
   end
 end
