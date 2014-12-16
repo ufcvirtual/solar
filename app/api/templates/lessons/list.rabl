d55a6bf518089d946a545ca4f5c980c20e669429
Wedson Lima <wedson.sousa.lima@gmail.com>
2014-12-16 16:08:53 -0300

    [#84039442] api / permissao de acesso / aulas e material

--- app/api/templates/lessons/list.rabl
+++ app/api/templates/lessons/list.rabl
@@ -12,7 +12,7 @@ attributes :id, :name, :description, :order, :is_default
         status: lesson.status,
         type: lesson.type_info,
         name: lesson.name,
-        url: (lesson.is_link? ? lesson.link_path : "/api/v1/lessons/#{lesson.id}/#{lesson.address}"),
+        url: (lesson.is_link? ? lesson.link_path : "/api/v1/groups/#{@group_id}/lessons/#{lesson.id}/#{lesson.address}"),
         start_date: schedule.start_date,
         end_date: schedule.end_date
       }
--- app/api/v1/support_material_files.rb
+++ app/api/v1/support_material_files.rb
@@ -1,18 +1,20 @@
 module V1
   class SupportMaterialFiles < Base
 
-    guard_all!
-
     namespace :groups do
+
+      before do
+        @ats = RelatedTaggable.related(group_id: params[:id])
+      end
 
       ## api/v1/groups/1/lessons
       desc "Lista de material de apoio da turma"
       params { requires :id, type: Integer, desc: "ID da turma" }
       get ":id/support_material_files" do
-      # get ":id/support_material_files", rabl: "support_material_files/list" do
-        @ats = RelatedTaggable.related(group_id: params[:id])
-        @material_files = SupportMaterialFile.list(@ats)
+        guard!
 
+        # get ":id/support_material_files", rabl: "support_material_files/list" do
+        @material_files = SupportMaterialFile.list(@ats)
         @material_files.map do |folder, files|
           {
             folder_name: folder,
@@ -21,22 +23,23 @@ module V1
                 id: file.id,
                 type: file.type_info,
                 name: file.name,
-                url: file.url || "/api/v1/support_material_files/#{file.id}/download"
+                url: file.url || "/api/v1/groups/#{params[:id]}/support_material_files/#{file.id}/download"
               }
             end
           }
         end
-      end
+      end # get
 
-    end # namespace
-
-    namespace :support_material_files do
       desc "Download material de apoio"
       params { requires :id, type: Integer, desc: "ID do material de apoio" }
-      get ":id/download" do
-        file = SupportMaterialFile.find(params[:id])
+      get ":id/support_material_files/:file_id/download" do
+        file = SupportMaterialFile.find(params[:file_id])
+
+        authorize! :download, SupportMaterialFile, on: @ats, read: true
+
         send_file(file.attachment.path.to_s, file.attachment_file_name.to_s)
-      end
+      end # get download
+
     end # namespace
 
   end
