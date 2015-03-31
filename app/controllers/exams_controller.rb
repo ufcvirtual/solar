190e70c507477c6fd230f6b7e17b419a2dfd797d
Patricia de Sousa Paula <patricia@virtual.ufc.br>
2015-03-31 17:14:28 -0300

    [#90121558] trabalhando na listagem de prova...

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -8,15 +8,13 @@ class ExamsController < ApplicationController
   #before_filter :get_groups_by_allocation_tags, only: [:new, :create]
 
   #before_filter only: [:edit, :update, :show] do |controller|
-  #  get_groups_by_tool(@discussion = Discussion.find(params[:id]))
+  #  get_groups_by_tool(@exam = Exam.find(params[:id]))
   #end
 
   def index
-    @allocation_tags_ids = params[:groups_by_offer_id].present? ? AllocationTag.at_groups_by_offer_id(params[:groups_by_offer_id]) : params[:allocation_tags_ids]
-    #authorize! :index, Exam, on: @allocation_tags_ids
-
-    @exams = Exam.joins(:schedule, academic_allocations: :allocation_tag).where(allocation_tags: {id: @allocation_tags_ids.split(" ").flatten}).uniq.select("exams.*, schedules.start_date AS start_date").order("start_date")
-
+    @allocation_tags_id = active_tab[:url][:allocation_tag_id]
+    authorize! :index, Exam, on: [@allocation_tags_id]
+    @exams = Exam.my_exams(@allocation_tags_id)
   #rescue
     #render json: {success: false, alert: t(:no_permission)}, status: :unauthorized
   end
--- app/models/exam.rb
+++ app/models/exam.rb
@@ -7,4 +7,12 @@ class Exam < Event
 
   has_many :exam_questions, dependent: :destroy
   has_many :questions, through: :exam_questions
+
+  def self.my_exams(allocation_tag_id)
+  	exams = Exam.joins(:academic_allocations, :schedule)
+      .where(academic_allocations: {allocation_tag_id: allocation_tag_id})
+      .select('DISTINCT exams.*, schedules.start_date as start_date, schedules.end_date as end_date')
+      .order('schedules.start_date')
+  end
+
 end
