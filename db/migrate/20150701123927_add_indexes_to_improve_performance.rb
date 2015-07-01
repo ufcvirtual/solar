0169fd24320494b4847f44638adc5c6d975fdd07
Bianca <bianca.stephani@gmail.com>
2015-07-01 10:26:18 -0300

    [#master] Comentando criação de indexes - criados direto no banco

--- db/migrate/20150701123927_add_indexes_to_improve_performance.rb
+++ db/migrate/20150701123927_add_indexes_to_improve_performance.rb
@@ -1,23 +1,24 @@
 class AddIndexesToImprovePerformance < ActiveRecord::Migration
   def up
-    ActiveRecord::Base.connection.execute <<-SQL
-      CREATE INDEX group_assignments_academic_allocation_id_idx ON group_assignments (academic_allocation_id);
-      CREATE INDEX assignment_comments_sent_assignment_id_idx   ON assignment_comments (sent_assignment_id);
-      CREATE INDEX comment_files_assignment_comment_id_idx      ON comment_files (assignment_comment_id);
+    # run sql directly on database
+    # ActiveRecord::Base.connection.execute <<-SQL
+    #   CREATE INDEX group_assignments_academic_allocation_id_idx ON group_assignments (academic_allocation_id);
+    #   CREATE INDEX assignment_comments_sent_assignment_id_idx   ON assignment_comments (sent_assignment_id);
+    #   CREATE INDEX comment_files_assignment_comment_id_idx      ON comment_files (assignment_comment_id);
 
-      CREATE INDEX academic_allocations_tool_id_type_allocation_tag_id_idx ON academic_allocations (allocation_tag_id, academic_tool_id, academic_tool_type);
-      DROP INDEX academic_tool_type_idx;
-      DROP INDEX academic_tool_id_idx;
-      DROP INDEX allocation_tag_id_idx;
+    #   CREATE INDEX academic_allocations_tool_id_type_allocation_tag_id_idx ON academic_allocations (allocation_tag_id, academic_tool_id, academic_tool_type);
+    #   DROP INDEX academic_tool_type_idx;
+    #   DROP INDEX academic_tool_id_idx;
+    #   DROP INDEX allocation_tag_id_idx;
 
-      CREATE INDEX user_messages_user_id_message_id_idx ON user_messages (user_id, message_id);
+    #   CREATE INDEX user_messages_user_id_message_id_idx ON user_messages (user_id, message_id);
 
-      CREATE INDEX lessons_lmodule_id ON lessons (lesson_module_id);
-      CREATE INDEX allocations_profile_user_allocation_tag_id ON allocations (profile_id, user_id, allocation_tag_id) WHERE status = 1;
+    #   CREATE INDEX lessons_lmodule_id ON lessons (lesson_module_id);
+    #   CREATE INDEX allocations_profile_user_allocation_tag_id ON allocations (profile_id, user_id, allocation_tag_id) WHERE status = 1;
 
-      DROP INDEX start_date_idx;
-      DROP INDEX end_date_idx;
-    SQL
+    #   DROP INDEX start_date_idx;
+    #   DROP INDEX end_date_idx;
+    # SQL
   end
 
   def down
