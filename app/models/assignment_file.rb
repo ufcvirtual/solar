b3e2ff1bfd3d3cad69161f26b2e5ecd44b1052df
Bianca <bianca.stephani@gmail.com>
2016-01-13 11:55:03 -0300

    [#master] Aumentando limite de arquivos enviados por alunos

--- app/models/assignment_file.rb
+++ app/models/assignment_file.rb
@@ -15,7 +15,7 @@ class AssignmentFile < ActiveRecord::Base
     path: ":rails_root/media/assignment/sent_assignment_files/:id_:basename.:extension",
     url: "/media/assignment/sent_assignment_files/:id_:basename.:extension"
 
-  validates_attachment_size :attachment, less_than: 5.megabyte, message: ' '
+  validates_attachment_size :attachment, less_than: 26.megabyte, message: ' '
   validates_attachment_content_type_in_black_list :attachment
 
   default_scope order: 'attachment_updated_at DESC'
