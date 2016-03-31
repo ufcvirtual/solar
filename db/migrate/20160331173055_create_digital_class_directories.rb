a9566f01035fb4c3746903a5aaab91bd2342c004
biancastephani <bianca.stephani@gmail.com>
2016-03-31 20:20:38 -0300

    Adicionando timestamp aos diretorios dc

--- db/migrate/20160331173055_create_digital_class_directories.rb
+++ db/migrate/20160331173055_create_digital_class_directories.rb
@@ -3,6 +3,7 @@ class CreateDigitalClassDirectories < ActiveRecord::Migration
     create_table :digital_class_directories do |t|
       t.integer :directory_id, index: true, null: false
       t.integer :related_taggable_id, index: true, null: false
+      t.timestamps
     end
   end
 end
