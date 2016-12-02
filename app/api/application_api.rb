d9cd2e3a658c942671ed858b42ee53761a487fcb
Bianca <bianca.stephani@gmail.com>
2016-12-02 09:00:07 -0300

    [#135130991] Adicionando internacionalizações, horário nos logs da api, tutoriais

--- app/api/application_api.rb
+++ app/api/application_api.rb
@@ -5,26 +5,26 @@ class ApplicationAPI < Grape::API
   formatter :json, Grape::Formatter::Rabl
 
   rescue_from ActiveRecord::RecordNotFound do |error|
-    Rails.logger.info "[API] [ERROR] [#{env["REQUEST_METHOD"]} #{env["PATH_INFO"]}] [404] message: #{error}"
+    Rails.logger.info "[API] [ERROR] [#{Time.now}] [#{env["REQUEST_METHOD"]} #{env["PATH_INFO"]}] [404] message: #{error}"
     rack_response(error.as_json , 404)
   end
 
   rescue_from Grape::Exceptions::ValidationErrors do |error|
-    Rails.logger.info "[API] [ERROR] [#{env["REQUEST_METHOD"]} #{env["PATH_INFO"]}] [400] message: #{error}"
+    Rails.logger.info "[API] [ERROR] [#{Time.now}] [#{env["REQUEST_METHOD"]} #{env["PATH_INFO"]}] [400] message: #{error}"
     rack_response(error.as_json, 400)
   end
 
   rescue_from CanCan::AccessDenied do |error|
-    Rails.logger.info "[API] [ERROR] [#{env["REQUEST_METHOD"]} #{env["PATH_INFO"]}] [401] message: #{error}"
+    Rails.logger.info "[API] [ERROR] [#{Time.now}] [#{env["REQUEST_METHOD"]} #{env["PATH_INFO"]}] [401] message: #{error}"
     rack_response(error.as_json, 401)
   end
 
   rescue_from :all do |error|
-    Rails.logger.info "[API] [ERROR] [#{env["REQUEST_METHOD"]} #{env["PATH_INFO"]}] [422] message: #{error}"
+    Rails.logger.info "[API] [ERROR] [#{Time.now}] [#{env["REQUEST_METHOD"]} #{env["PATH_INFO"]}] [422] message: #{error}"
     rack_response(error.as_json, 422)
   end
 
-  before { Rails.logger.info "[API] [INFO] [#{env["REQUEST_METHOD"]} #{env["PATH_INFO"]}] [#{request.headers['Solar']}] params: #{ActionController::Parameters.new(params).except("route_info", "access_token").as_json}" }
+  before { Rails.logger.info "[API] [INFO] [#{Time.now}] [#{env["REQUEST_METHOD"]} #{env["PATH_INFO"]}] [#{request.headers['Solar']}] params: #{ActionController::Parameters.new(params).except("route_info", "access_token").as_json}" }
 
   helpers Helpers::V1::All
   mount V1::Base
