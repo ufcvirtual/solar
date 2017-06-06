06f8b59aa0c8d3e6b7b6b07e020c1ed714d6c6a8
Bianca <bianca.stephani@gmail.com>
2017-06-06 11:59:23 -0300

    [#master] Removendo spec/factories

--- spec/factories/semesters.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :semester do
-    name "MyString"
-    offer_schedule_id nil
-    enrollment_schedule_id nil
-  end
-end
