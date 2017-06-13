432cf25d2631bc231997e599f88fb0e0b2cacd24
Cristiano Lima <cristiano@virtual.ufc.br>
2017-06-13 10:57:05 -0300

    [#76911972] Adiciona tipo outros na agenda

--- app/models/event.rb
+++ app/models/event.rb
@@ -67,7 +67,7 @@ class Event < ActiveRecord::Base
               when Presential_Test; 'presential_test'
               when Presential_Meeting; 'presential_meeting'
               when WebConferenceLesson; 'web_conference_lesson'
-              else 'recess_or_holiday'; end
+              else 'recess_or_holiday_or_other'; end
     end
     api_t
   end
@@ -81,7 +81,7 @@ class Event < ActiveRecord::Base
     when 'presential_test'; '#F5D5EF'
     when 'presential_meeting'; '#FFD9E0'
     when 'web_conference_lesson'; '#F5DA81'
-    when 'recess_or_holiday'; '#E3E3E3'
+    when 'recess_or_holiday_or_other'; '#E3E3E3'
     else
       '#CCCCFF'
     end
