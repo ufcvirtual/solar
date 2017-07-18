0f8299bc40a598dc9e3923b49bf3cec6472e6cab
Bianca <bianca.stephani@gmail.com>
2017-07-18 12:33:42 -0300

    [#148608257] Ajeitando identação

--- vendor/assets/javascripts/ckeditor/config.js
+++ vendor/assets/javascripts/ckeditor/config.js
@@ -5,20 +5,18 @@ CKEDITOR.on( 'dialogDefinition', function( ev ) {
   if ( dialogName == 'link' ) { // Se for a janela de "Link"
     dialogDefinition.removeContents('advanced'); // Remove aba de configurações avançadas
 
-	if (ev.data.name === 'link') {
-        var target = ev.data.definition.getContents('target');
-        var options = target.get('linkTargetType').items;
-        for (var i = options.length-1; i >= 0; i--) {
-            var label = options[i][0];
-            if (!label.match(/_blank/i)) {
-                options.splice(i, 1);
-            }
+    var target = ev.data.definition.getContents('target');
+    var options = target.get('linkTargetType').items;
+    for (var i = options.length-1; i >= 0; i--) {
+        var label = options[i][0];
+        if (!label.match(/_blank/i)) {
+            options.splice(i, 1);
         }
-        var targetField = target.get( 'linkTargetType' );
-        targetField['default'] = '_blank';
     }
+    var targetField = target.get( 'linkTargetType' );
+    targetField['default'] = '_blank';
 
- 	//    var infoTab = dialogDefinition.getContents( 'target'); // Recupera as informações da aba de destino do link
- 	//    infoTab.elements[0].children[0].default = '_blank'; // Seta como o padrão sendo "_blank"
+    //    var infoTab = dialogDefinition.getContents( 'target'); // Recupera as informações da aba de destino do link
+    //    infoTab.elements[0].children[0].default = '_blank'; // Seta como o padrão sendo "_blank"
   }
 });
