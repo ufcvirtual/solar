d455fc3d07463920bb1de9c824fd3411fe7843ec
LeeboyOver9000 <pedro.ivo.freire.aragao@gmail.com>
2017-08-08 09:15:24 -0300

    [#147095371] Adicionando registro de log para os delayed jobs

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,5 +1,7 @@
 Delayed::Worker.destroy_failed_jobs = false
-Delayed::Worker.sleep_delay = 60 # If no jobs are found, the worker sleeps for 60 second
+Delayed::Worker.sleep_delay = 60 # If no jobs are available, the worker sleeps for 60 second
 Delayed::Worker.max_attempts = 3
 Delayed::Worker.max_run_time = 5.minutes
 Delayed::Worker.default_queue_name = 'default'
+Delayed::Worker.raise_signal_exceptions = :term
+Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
