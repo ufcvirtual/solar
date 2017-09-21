b5ed3707ec63a1b326a4e0a8e05f58a609afc261
LeeboyOver9000 <pedro.ivo.freire.aragao@gmail.com>
2017-09-21 17:08:06 -0300

    [#142314227] Inserindo atalhos para acessar o menu lateral

--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -23,10 +23,14 @@ module MenuHelper
       parents: {}
     }
 
+    letters_to_shortcut = []
     menus.each_with_index do |menu, idx|
       contexts = menu.contexts.pluck(:id)
+      count = 0
+      count += 1 while letters_to_shortcut.include?(letter_shortcut_sanitize(t(menu.name)[count].downcase))
+      letters_to_shortcut << letter_shortcut_sanitize(t(menu.name)[count].downcase)
       menu_item_link = link_to(t(menu.name), url_for({controller: "/#{menu.resource.controller}", action: menu.resource.action,
-        bread: menu.name, contexts: contexts.join(',')}), onclick: 'focusTitle();', onkeypress: 'focusTitle();', onkeydown: 'click_on_keypress(event, this);', class: menu.parent.nil? ? 'mysolar_menu_title' : '')
+        bread: menu.name, contexts: contexts.join(',')}), onclick: 'focusTitle();', onkeypress: 'focusTitle();', onkeydown: 'click_on_keypress(event, this);', class: menu.parent.nil? ? 'mysolar_menu_title' : '', :'data-shortcut' => (letter_shortcut_sanitize(t(menu.name)[count].downcase).ord - 32), :'data-shortcut-shift' => true)
       menu_item = {contexts: contexts, bread: menu.name, link: menu_item_link}
 
       if menu.parent.nil?
@@ -43,17 +47,13 @@ module MenuHelper
   private
 
     def to_html(menu_list)
-      letters_to_shortcut = []
       parents = menu_list[:parents].map do |p_id, father|
-        count = 0
-        count += 1 while letters_to_shortcut.include?(father[:name][count].downcase)
-        letters_to_shortcut << father[:name][count].downcase
         children = father[:children].map { |kid| create_link(kid, p_id, 'list') }
         %{
           <div class="mysolar_menu_group">
             <ul>
               <li class="mysolar_menu_title_multiple">
-                <span data-shortcut="#{father[:name][count].downcase.ord - 32}" data-shortcut-focus="true" data-shortcut-shift="true">#{father[:name]}</span>
+                #{father[:name]}
                 <ul class="submenu">
                   #{children.join}
                 </ul>
@@ -83,6 +83,16 @@ module MenuHelper
           #{link[:link]}
         </li>
       }
+    end
+
+    def letter_shortcut_sanitize(letter)
+      return "c" if letter == "ç"
+      return "i" if letter == "í"
+      return "u" if letter == "ú"
+      return "e" if letter == "é" || letter == "ẽ"
+      return "o" if letter == "ó" || letter == "õ"
+      return "a" if letter == "á" || letter == "ã" || letter == "à"
+      return letter
     end
 
 end # end module
