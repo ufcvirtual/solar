ecf8cff8ebf3d224bbec36ab5a1286a2ee6107e5
Bianca <bianca.stephani@gmail.com>
2017-09-26 12:40:13 -0300

    [#151098024] Atalho de comentário no fórum; criação de task rake pro grape; alteração de texto na ajuda contextual; método na API para visualizar os comentários de um aluno em uma atividade

--- app/controllers/access_control_controller.rb
+++ app/controllers/access_control_controller.rb
@@ -39,17 +39,22 @@ class AccessControlController < ApplicationController
     end
   end
 
-  def comments
+  def comment_media
+    guard_with_access_token_or_authenticate
+
     file = CommentFile.find(params[:file].split('_')[0])
     acu = file.comment.academic_allocation_user
+    user_id = current_user.try(:id) || User.current.id
 
-    is_observer_or_responsible = acu.allocation_tags.first.is_observer_or_responsible?(current_user.id)
+    is_observer_or_responsible = acu.allocation_tag.is_observer_or_responsible?(user_id)
 
-    unless acu.user_id == current_user.id || is_observer_or_responsible
-      raise CanCan::AccessDenied unless (acu.academic_allocation.academic_tool_type == 'Assignment' && !acu.group_assignment.blank? && acu.group_assignment.user_in_group?(current_user.id))
+    unless acu.user_id == user_id || is_observer_or_responsible
+      raise CanCan::AccessDenied unless (acu.academic_allocation.academic_tool_type == 'Assignment' && !acu.group_assignment.blank? && acu.group_assignment.user_in_group?(user_id))
     end
 
-    send_file(file.attachment.path, { disposition: 'inline', type: return_type(params[:extension])})
+    send_file(file.attachment.path, { disposition: 'inline', type: return_type(params[:extension] || file.attachment.path.split('.').last)})
+  rescue 
+    raise CanCan::AccessDenied
   end
 
   def bibliography
