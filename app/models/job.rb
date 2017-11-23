8e4fbf53432725c1581badbf589eade85e32e19d
Cristiano Lima <cristiano@virtual.ufc.br>
2017-11-23 16:06:53 -0300

    [#152235291] Correção e comentários na class Job

--- app/models/job.rb
+++ app/models/job.rb
@@ -5,6 +5,13 @@ class Job
  #soma a quantidade a ser enviada com a quantidade enviada nos ultimos 30 minutos
  #altera o status dos Jobs que seram enviados
  #quantidade maxima de email que pode ser enviado é 500 está setado no select do update  
+ #O tempo de espera está setado no select como 30 minutos (interval '30 minute')
+ #Exemplo de como enviar pacote com 3 email a cada dois minutos
+    #altera o tempo de 30 minutos do select para 2 minutos
+    #alterar o valor 500 do select do update para 3
+    #alterar o max_size do metodo send_mass_email para 3
+    #alterar o scheduler para roda a cada 2 minutos
+    #PARA O PROCESSO DO DELAYED JOB: script/delayed_job stop
   def self.select_and_update_status_jobs_not_send
     Delayed::Job.find_by_sql <<-SQL
         WITH RECURSIVE delayed_job(id, amount, total) AS (
@@ -15,7 +22,7 @@ class Job
             FROM delayed_jobs dj2 WHERE dj2.id <= dj1.id AND dj2.status = false) AS total
           FROM delayed_jobs dj1 WHERE dj1.status = false ORDER BY id ASC
         )
-        UPDATE delayed_jobs SET status = true WHERE id IN (SELECT id from delayed_job WHERE total<500) RETURNING *;
+        UPDATE delayed_jobs SET status = true WHERE id IN (SELECT id from delayed_job WHERE total<=500) RETURNING *;
     SQL
   end  
 
