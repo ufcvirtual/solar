df6eae273804ff71aae74f8176cc6e4a0b17394d
Bianca <bianca.stephani@gmail.com>
2017-12-06 08:29:19 -0300

    [#134864743] Corrigindo layout fancybox material de apoio, recolocando codigo para abrir pdfs

--- app/controllers/access_control_controller.rb
+++ app/controllers/access_control_controller.rb
@@ -63,7 +63,21 @@ class AccessControlController < ApplicationController
 
   def support_material_file
     get_file(SupportMaterialFile, 'support_material_files')
-  end 
+  end
+
+  def support_material
+    unless Exam.verify_blocking_content(current_user.id)
+      file = SupportMaterialFile.find(params[:path].split('_')[0])
+      if file.is_file?
+        file_path = File.join(SupportMaterialFile::FILES_PATH, [params[:path], '.', file.name.split('.').last ].join)
+        File.exist?(file_path) ? send_file(file_path, disposition: 'inline') : render(nothing: true) 
+      else
+        path = file.path(true)
+        params[:extension] = path.split('.').last if params[:extension].nil?
+        send_file(path, { disposition: 'inline', type: return_type(params[:extension]) })
+      end
+    end  
+  end
 
   def question_image 
     question = QuestionImage.find(params[:file].split('_')[0]).question
