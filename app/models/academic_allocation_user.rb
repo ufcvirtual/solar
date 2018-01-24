fd0c045e60a8ecde639445801b6cdd171108b017
LeeboyOver9000 <pedro.ivo.freire.aragao@gmail.com>
2018-01-24 12:36:42 -0300

    Colocando valores decimais na frequencia

--- app/models/academic_allocation_user.rb
+++ app/models/academic_allocation_user.rb
@@ -30,7 +30,7 @@ class AcademicAllocationUser < ActiveRecord::Base
   validates :user_id, uniqueness: { scope: [:group_assignment_id, :academic_allocation_id] }
   validates :user_id, presence: true, if: 'group_assignment_id.nil?'
   validates :grade, numericality: { greater_than_or_equal_to: 0, smaller_than_or_equal_to: 10, allow_blank: true }, unless: 'grade.blank?'
-  validates :working_hours, numericality: { greater_than_or_equal_to: 0,  only_integer: true, allow_blank: true }, unless: 'working_hours.blank?'
+  validates :working_hours, numericality: { greater_than_or_equal_to: 0, allow_blank: true }, unless: 'working_hours.blank?'
   validate :verify_wh, if: '!working_hours.blank? && merge.nil?'
   validate :verify_grade, if: '!grade.blank? && merge.nil?'
   validate :verify_offer, :verify_date, if: '(working_hours_changed? || grade_changed?) && merge.nil?'
@@ -54,7 +54,7 @@ class AcademicAllocationUser < ActiveRecord::Base
 
   def verify_wh
     if !academic_allocation.frequency
-      errors.add(:working_hours, I18n.t('academic_allocation_users.errors.not_frequency')) 
+      errors.add(:working_hours, I18n.t('academic_allocation_users.errors.not_frequency'))
     else
       errors.add(:working_hours, I18n.t('academic_allocation_users.errors.lower_than_max', max: academic_allocation.max_working_hours)) if working_hours > academic_allocation.max_working_hours
     end
@@ -75,7 +75,7 @@ class AcademicAllocationUser < ActiveRecord::Base
 
   def verify_offer
     offer_active = allocation_tag.offers.first.is_active?
-    
+
     errors.add(:grade, I18n.t('academic_allocation_users.errors.offer')) if academic_allocation.evaluative && grade_changed? && !offer_active
     errors.add(:working_hours, I18n.t('academic_allocation_users.errors.offer')) if academic_allocation.frequency && working_hours_changed? && !offer_active
   end
@@ -122,7 +122,7 @@ class AcademicAllocationUser < ActiveRecord::Base
       allocation = allocations.where('final_grade IS NOT NULL OR working_hours IS NOT NULL').first || allocations.first
 
       allocation.calculate_working_hours
-      
+
       allocation.calculate_parcial_grade unless academic_allocation.final_exam
       allocation.calculate_final_exam_grade
     end
@@ -182,7 +182,7 @@ class AcademicAllocationUser < ActiveRecord::Base
         if acu.grade.blank? && acu.working_hours.blank?
           acu.update_attributes status: status
         else
-          acu.update_attributes new_after_evaluation: new_object 
+          acu.update_attributes new_after_evaluation: new_object
         end
       end
 
@@ -197,7 +197,7 @@ class AcademicAllocationUser < ActiveRecord::Base
       acu.update_attributes new_after_evaluation: new_object
     end
     acu
-  end 
+  end
 
   def self.set_new_after_evaluation(allocation_tag_id, tool_id, tool_type, users_ids, group_id=nil, new_object=false)
     AcademicAllocationUser.joins(:academic_allocation).where(academic_allocations: {academic_tool_id: tool_id, academic_tool_type: tool_type, allocation_tag_id: allocation_tag_id}, user_id: (group_id.nil? ? users_ids : nil), group_assignment_id: group_id).update_all new_after_evaluation: false
@@ -206,7 +206,7 @@ class AcademicAllocationUser < ActiveRecord::Base
   def self.get_grade_and_wh(user_id, tool, tool_id, at)
     acu = joins(:academic_allocation).where(academic_allocations: {academic_tool_id: tool_id, academic_tool_type: tool, allocation_tag_id: at}, user_id: user_id).last
     {grade: acu.try(:grade), wh: acu.try(:working_hours)}
-  end 
+  end
 
   def self.any_evaluated?(ats, tool_id=nil, tool_type=nil)
     query = { academic_allocations: {allocation_tag_id: ats} }
@@ -243,7 +243,7 @@ class AcademicAllocationUser < ActiveRecord::Base
 
   def delete_with_dependents
     comments.map(&:delete_with_dependents)
-    
+
     case academic_allocation.academic_tool_type
     when 'Exam'
       # do nothing
@@ -313,23 +313,23 @@ class AcademicAllocationUser < ActiveRecord::Base
 
   def info
     case academic_allocation.academic_tool_type
-    when 'Assignment' 
+    when 'Assignment'
       grade, working_hours, comments = try(:grade), try(:working_hours), try(:comments)
-    
+
       files = AcademicAllocationUser.find_by_sql <<-SQL
         SELECT MAX(max_date) FROM (
-          SELECT MAX(initial_time) AS max_date FROM assignment_webconferences 
+          SELECT MAX(initial_time) AS max_date FROM assignment_webconferences
           WHERE academic_allocation_user_id = #{id}
 
           UNION
 
-          SELECT MAX(attachment_updated_at) AS max_date FROM assignment_files 
-          WHERE attachment_updated_at IS NOT NULL 
+          SELECT MAX(attachment_updated_at) AS max_date FROM assignment_files
+          WHERE attachment_updated_at IS NOT NULL
           AND academic_allocation_user_id = #{id}
         ) AS max;
 
       SQL
- 
+
       has_files = !files.first.max.nil?
 
       { grade: grade, working_hours: working_hours, comments: comments, has_files: has_files, file_sent_date: (has_files ? I18n.l(files.first.max.to_datetime, format: :normal) : ' - ') }
@@ -346,7 +346,7 @@ class AcademicAllocationUser < ActiveRecord::Base
 
   def self.get_grade_finish(user_id, at_id)
     Allocation.where(user_id: user_id, allocation_tag_id: at_id).last
-  end 
+  end
 
   def can_evaluate?
     return true unless academic_allocation.final_exam
