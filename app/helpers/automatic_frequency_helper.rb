6b3f2cdb6c3b9431640629465af4a9f6aba8e93c
LeeboyOver9000 <pedro.ivo.freire.aragao@gmail.com>
2018-02-20 11:37:51 -0300

    [#152251973] Correção de frequência automática ao entrar na webconferência

--- app/helpers/automatic_frequency_helper.rb
+++ app/helpers/automatic_frequency_helper.rb
@@ -11,30 +11,57 @@ module AutomaticFrequencyHelper
   private
 
     def automatic_frequency(type, activity)
-      unless activity.nil? || activity.academic_allocation_user_id.nil?
-        academic_allocation_user = activity.class.to_s == "AcademicAllocationUser" ? activity : AcademicAllocationUser.find(activity.academic_allocation_user_id)
-        academic_allocation = AcademicAllocation.find(academic_allocation_user.academic_allocation_id)
+      if !activity.nil?
+        if activity.class.to_s == "AcademicAllocationUser"
+          academic_allocation_user = activity
+          academic_allocation = AcademicAllocation.find(academic_allocation_user.academic_allocation_id)
 
-        if academic_allocation.frequency_automatic && !academic_allocation_user.evaluated_by_responsible
+          if academic_allocation.frequency_automatic && !academic_allocation_user.evaluated_by_responsible
 
-          if activity.class.to_s != "AcademicAllocationUser"
-            query = { academic_allocation_user_id: activity.academic_allocation_user_id }
-            query.merge!( {draft: false} ) if activity.class.to_s == "Post"
-            activities = activity.class.where(query)
-          end
-
-          if type == "set_frequency"
-            if activity.class.to_s == "Post" && !activity.draft
-              academic_allocation_user.working_hours = academic_allocation.max_working_hours
-            else
-              academic_allocation_user.working_hours = academic_allocation.max_working_hours
+            if activity.class.to_s != "AcademicAllocationUser"
+              query = { academic_allocation_user_id: activity.academic_allocation_user_id }
+              query.merge!( {draft: false} ) if activity.class.to_s == "Post"
+              activities = activity.class.where(query)
             end
-          elsif type == "remove_frequency" && activities.size < 1
-            academic_allocation_user.working_hours = nil
-          end
 
-          academic_allocation_user.save
+            if type == "set_frequency"
+              if activity.class.to_s == "Post" && !activity.draft
+                academic_allocation_user.working_hours = academic_allocation.max_working_hours
+              else
+                academic_allocation_user.working_hours = academic_allocation.max_working_hours
+              end
+            elsif type == "remove_frequency" && activities.size < 1
+              academic_allocation_user.working_hours = nil
+            end
+
+            academic_allocation_user.save
+          end
+        elsif !activity.academic_allocation_user_id.nil?
+          academic_allocation_user = AcademicAllocationUser.find(activity.academic_allocation_user_id)
+          academic_allocation = AcademicAllocation.find(academic_allocation_user.academic_allocation_id)
+
+          if academic_allocation.frequency_automatic && !academic_allocation_user.evaluated_by_responsible
+
+            if activity.class.to_s != "AcademicAllocationUser"
+              query = { academic_allocation_user_id: activity.academic_allocation_user_id }
+              query.merge!( {draft: false} ) if activity.class.to_s == "Post"
+              activities = activity.class.where(query)
+            end
+
+            if type == "set_frequency"
+              if activity.class.to_s == "Post" && !activity.draft
+                academic_allocation_user.working_hours = academic_allocation.max_working_hours
+              else
+                academic_allocation_user.working_hours = academic_allocation.max_working_hours
+              end
+            elsif type == "remove_frequency" && activities.size < 1
+              academic_allocation_user.working_hours = nil
+            end
+
+            academic_allocation_user.save
+          end
         end
+
       end
     end
 
