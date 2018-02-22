c29475a8178e623227e12449f5e669e057d8863d
Bianca <bianca.stephani@gmail.com>
2018-02-22 12:08:06 -0300

    [#154602185] Permitindo emails nulos ou duplicados para usuários integrados e não desvinculados

--- app/controllers/devise/users_passwords_controller.rb
+++ app/controllers/devise/users_passwords_controller.rb
@@ -2,11 +2,22 @@ class Devise::UsersPasswordsController < Devise::PasswordsController
   include SysLog::Devise
 
   def create
-    user = User.find_by_email(params[:user][:email])
-    if !(user.nil?) && user.integrated?
-      redirect_to login_path, notice: t("devise.users_passwords.ma_request").html_safe
+    user = User.find_by_cpf(params[:user][:cpf].delete('.-'))
+    if user.blank?
+      redirect_to login_path, alert: t("devise.users_passwords.not_found").html_safe
+    elsif user.integrated? && !user.on_blacklist?
+      unless (user.selfregistration)
+        redirect_to login_path, alert: t("users.errors.ma.selfregistration").html_safe
+      else
+        redirect_to login_path, notice: t("devise.users_passwords.ma_request").html_safe
+      end
     else
-      super
+      if user.email.blank?
+        redirect_to login_path, notice: t("devise.users_passwords.no_email").html_safe
+      else
+        user.send_reset_password_instructions
+        redirect_to login_path, notice: t("devise.users_passwords.send_instructions_email", email: user.email).html_safe
+      end
     end
   end
 end
