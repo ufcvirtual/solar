cd9214698bb0198de4ea2adb21b563c4e4d4654a
LeeboyOver9000 <pedro.ivo.freire.aragao@gmail.com>
2018-03-15 12:23:00 -0300

    [#155653825] Resolvido o problema dos chats duplicados

--- app/models/concerns/academic_tool.rb
+++ app/models/concerns/academic_tool.rb
@@ -262,7 +262,7 @@ module AcademicTool
 
     def define_academic_associations
       unless allocation_tag_ids_associations.blank?
-        academic_allocations.create allocation_tag_ids_associations.map {|at| { allocation_tag_id: at }}
+        academic_allocations.create allocation_tag_ids_associations.map {|at| { allocation_tag_id: at }} unless self.class.to_s == 'ChatRoom'
       else
         academic_allocations.create
       end
