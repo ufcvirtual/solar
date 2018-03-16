84043fe87fa968616d26a51954ae704ddc75892a
Danniel Albuquerque <danniel.master@gmail.com>
2018-03-16 16:50:11 -0300

    ajustes no método update_activities

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -66,17 +66,10 @@ class OffersController < ApplicationController
   def update
     @type_id =  params[:offer][:type_id].to_i
     @offer   = Offer.find(params[:id])
-<<<<<<< d2861e4175f7f96deb42ef646837d26b30dd942d
 
     optional_authorize(:update)
     update_offer_activities(@offer)
 
-=======
-
-    optional_authorize(:update)
-    update_offer_activities(@offer)
-
->>>>>>> teste inicial editando datas de atividades da oferta
     if @offer.update_attributes(offer_params)
       render json: { success: true, notice: t(:updated, scope: [:offers, :success]) }
     else
@@ -200,6 +193,7 @@ class OffersController < ApplicationController
               activities_to_email[Object.const_get(al.academic_tool_type).model_name.human] ||= []
               activities_to_email[Object.const_get(al.academic_tool_type).model_name.human] << struct
               activities_to_save << act
+
             end
 
           end
