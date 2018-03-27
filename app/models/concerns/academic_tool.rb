4d76fa6db8b28533b58b489978a1a574961536f7
Bianca <bianca.stephani@gmail.com>
2018-03-27 09:29:01 -0300

    [#155653825] Adicionando comentário explicando a correção nos chats

--- app/models/concerns/academic_tool.rb
+++ app/models/concerns/academic_tool.rb
@@ -264,7 +264,8 @@ module AcademicTool
 
     def define_academic_associations
       unless allocation_tag_ids_associations.blank?
-        academic_allocations.create allocation_tag_ids_associations.map {|at| { allocation_tag_id: at }}
+        academic_allocations.create allocation_tag_ids_associations.map {|at| { allocation_tag_id: at }} unless self.class.to_s == 'ChatRoom'
+        # chat already creates academic_allocations on its model because of the line accepts_nested_attributes_for :academic_allocations
       else
         academic_allocations.create
       end
