189c9b8616d731d5f05399ddec0e672febe746ce
Leeboy <pedro.ivo.freire.aragao@gmail.com>
2018-05-07 16:54:20 -0300

    [#155394380] Corrigindo alguns bugs e inserindo autorização

--- app/controllers/schedule_event_files_controller.rb
+++ app/controllers/schedule_event_files_controller.rb
@@ -1,6 +1,7 @@
 class ScheduleEventFilesController < ApplicationController
 
-  # include SysLog::Actions
+  include SysLog::Actions
+  include FilesHelper
   include ScheduleEventFilesHelper
 
   before_action :set_current_user, only: [:destroy, :create]
@@ -17,20 +18,19 @@ class ScheduleEventFilesController < ApplicationController
   end
 
   def create
-    # authorize! :create, ScheduleEventFile, on: [@allocation_tag_id = active_tab[:url][:allocation_tag_id]]
+    authorize! :create, ScheduleEventFile, on: [@allocation_tag_id = active_tab[:url][:allocation_tag_id]]
 
-    @allocation_tag_id = active_tab[:url][:allocation_tag_id]
     # verify_owner!(schedule_event_file_params)
     create_many
     # set_ip_user
 
     render partial: 'files', locals: { files: @schedule_event_files, disabled: false }
-  # rescue ActiveRecord::AssociationTypeMismatch
-  #   render json: { success: false, alert: t(:not_associated) }, status: :unprocessable_entity
-  # rescue CanCan::AccessDenied
-  #   render json: { success: false, alert: t(:no_permission) }, status: :unauthorized
-  # rescue => error
-  #   render_json_error(error, 'schedule_event_files.error', (error == 'not_started_up' ? 'not_started_up' : 'new'))
+  rescue ActiveRecord::AssociationTypeMismatch
+    render json: { success: false, alert: t(:not_associated) }, status: :unprocessable_entity
+  rescue CanCan::AccessDenied
+    render json: { success: false, alert: t(:no_permission) }, status: :unauthorized
+  rescue => error
+    render_json_error(error, 'schedule_event_files.error', (error == 'not_started_up' ? 'not_started_up' : 'new'))
   end
 
   def destroy
@@ -38,33 +38,33 @@ class ScheduleEventFilesController < ApplicationController
     # set_ip_user
     @schedule_event_file.destroy
 
-    render json: { success: true, notice: t('assignment_files.success.removed') }
-  # rescue CanCan::AccessDenied
-  #   render json: { success: false, alert: t(:no_permission) }, status: :unauthorized
-  # rescue => error
-  #   render_json_error(error, 'assignment_files.error', 'remove')
+    render json: { success: true, notice: t('schedule_event_files.success.deleted') }
+  rescue CanCan::AccessDenied
+    render json: { success: false, alert: t(:no_permission) }, status: :unauthorized
+  rescue => error
+    render_json_error(error, 'schedule_event_files.error', 'remove')
   end
 
-  # def download
-  #   if Exam.verify_blocking_content(current_user.id)
-  #     redirect_to list_assignments_path, alert: t('assignments.restrict_assignment')
-  #   else
-  #     allocation_tag_id = active_tab[:url][:allocation_tag_id]
-  #
-  #     if params[:zip].present?
-  #       assignment = ScheduleEvent.find(params[:assignment_id])
-  #       academic_allocation_user = assignment.academic_allocation_users.where(user_id: params[:student_id], group_assignment_id: params[:group_id], academic_allocations: {allocation_tag_id: allocation_tag_id}).first
-  #       path_zip   = compress_file({ files: academic_allocation_user.assignment_files, table_column_name: 'attachment_file_name', name_zip_file: assignment.name })
-  #     else
-  #       file = ScheduleEventFile.find(params[:id])
-  #       academic_allocation_user = file.academic_allocation_user
-  #       path_zip  = file.attachment.path
-  #       file_name = file.attachment_file_name
-  #     end
-  #     raise CanCan::AccessDenied unless ScheduleEventFile.owned_by_user?(current_user.id, { academic_allocation_user: academic_allocation_user }) || AllocationTag.find(allocation_tag_id).is_observer_or_responsible?(current_user.id)
-  #     download_file(:back, path_zip, file_name)
-  #   end
-  # end
+  def download
+    # if Exam.verify_blocking_content(current_user.id)
+    #   redirect_to list_assignments_path, alert: t('assignments.restrict_assignment')
+    # else
+      allocation_tag_id = active_tab[:url][:allocation_tag_id]
+
+      if params[:zip].present?
+        assignment = ScheduleEvent.find(params[:assignment_id])
+        academic_allocation_user = assignment.academic_allocation_users.where(user_id: params[:student_id], group_assignment_id: params[:group_id], academic_allocations: {allocation_tag_id: allocation_tag_id}).first
+        path_zip   = compress_file({ files: academic_allocation_user.assignment_files, table_column_name: 'attachment_file_name', name_zip_file: assignment.name })
+      else
+        file = ScheduleEventFile.find(params[:id])
+        academic_allocation_user = file.academic_allocation_user
+        path_zip  = file.attachment.path
+        file_name = file.attachment_file_name
+      end
+      # raise CanCan::AccessDenied unless ScheduleEventFile.owned_by_user?(current_user.id, { academic_allocation_user: academic_allocation_user }) || AllocationTag.find(allocation_tag_id).is_observer_or_responsible?(current_user.id)
+      download_file(:back, path_zip, file_name)
+    # end
+  end
 
   private
 
