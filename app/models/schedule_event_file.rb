7887308ac13d0aecaa9ebe48e3571e1f87a82f1f
Leeboy <pedro.ivo.freire.aragao@gmail.com>
2018-05-09 12:49:15 -0300

    [#155394380] Validando arquivos de eventos ao inserir e ao remover

--- app/models/schedule_event_file.rb
+++ app/models/schedule_event_file.rb
@@ -8,28 +8,38 @@ class ScheduleEventFile < ActiveRecord::Base
   belongs_to :user
   belongs_to :academic_allocation_user, counter_cache: true
 
-  # before_save :can_change?, if: 'merge.nil?'
-  # before_destroy :can_destroy?
+  before_destroy :can_destroy?
+  before_save :replace_attachment_file_name
 
   validates :attachment, presence: true
   validates :academic_allocation_user_id, presence: true
 
   has_attached_file :attachment,
-    path: ":rails_root/media/schedule_event/schedule_event_files/:id_:basename.:extension",
-    url: "/media/schedule_event/schedule_event_files/:id_:basename.:extension"
+    path: ":rails_root/media/schedule_event/schedule_event_files/:id_:normalized_attachment_file_name",
+    url: "/media/schedule_event/schedule_event_files/:id_:normalized_attachment_file_name"
 
-  # validates_attachment_size :attachment_file, less_than: 26.megabyte, message: ' '
-  # validates_attachment_content_type_in_black_list :attachment
+  validates_attachment_size :attachment, less_than: 26.megabyte, message: I18n.t('schedule_event_files.error.attachment_file_size_too_big')
+  validates_attachment_content_type :attachment, content_type: /(^image\/(jpeg|jpg|gif|png)$)|\Aapplication\/pdf/, message: I18n.t('schedule_event_files.error.wrong_type')
 
-  def can_change?
-    raise 'date_range_expired' unless schedule_event.in_time?
+  Paperclip.interpolates :normalized_attachment_file_name do |attachment, style|
+    attachment.instance.normalized_attachment_file_name
+  end
+
+  def normalized_attachment_file_name
+    "#{self.academic_allocation_user.user.cpf}-#{self.attachment_file_name.gsub( /[^a-zA-Z0-9_\.]/, '_')}"
   end
 
   def can_destroy?
-    raise 'date_range_expired' unless schedule_event.in_time?
+    raise 'remove' unless academic_allocation_user.grade.nil? && academic_allocation_user.working_hours.nil? && academic_allocation_user.comments_count == 0
   end
 
   def delete_with_dependents
     self.delete
   end
+
+  private
+
+    def replace_attachment_file_name
+      self.attachment_file_name = normalized_attachment_file_name
+    end
 end
