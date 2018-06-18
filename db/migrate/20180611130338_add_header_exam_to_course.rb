b4b66640408704074f2a5185db604ae6f4a278e1
Leeboy <pedro.ivo.freire.aragao@gmail.com>
2018-06-18 15:46:07 -0300

    [#157780121] Inserindo ckeditor para padronização de cabeçalhos nas provas

---
+++ db/migrate/20180611130338_add_header_exam_to_course.rb
@@ -0,0 +1,11 @@
+class AddHeaderExamToCourse < ActiveRecord::Migration
+  def up
+    add_column :courses, :has_exam_header, :boolean, default: false, null: false
+    add_column :courses, :header_exam, :text
+  end
+
+  def down
+    remove_column :courses, :header_exam
+    remove_column :courses, :has_exam_header
+  end
+end
