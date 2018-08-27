96fce41bcb6d1a00e6a389db1615a4517273f83d
Bianca <bianca.stephani@gmail.com>
2018-08-27 14:15:06 -0300

    [#156228515] Refatorando código de validação dinâmica de anexos

--- app/models/public_file.rb
+++ app/models/public_file.rb
@@ -1,5 +1,7 @@
 class PublicFile < ActiveRecord::Base
 
+  FILESIZE = 20.megabyte
+
   before_destroy :can_remove?
 
   belongs_to :user
@@ -11,10 +13,10 @@ class PublicFile < ActiveRecord::Base
     path: ":rails_root/media/assignment/public_area/:id_:basename.:extension",
     url: "/media/assignment/public_area/:id_:basename.:extension"
 
-  validates_attachment_size :attachment, less_than: 20.megabyte, message: ""
+  validates_attachment_size :attachment, less_than: FILESIZE, message: ""
 
   validates_attachment_content_type_in_black_list :attachment
-  
+
   before_save :verify_offer, if: 'merge.nil?'
 
   #default_scope order: 'attachment_updated_at DESC'
@@ -32,7 +34,7 @@ class PublicFile < ActiveRecord::Base
     offer = AllocationTag.find(allocation_tag_id).offers.first
     if offer.end_date < Date.current
       errors.add(:base, I18n.t('public_files.error.offer_end')) if offer.end_date < Date.current
-      raise 'offer_end' 
+      raise 'offer_end'
     end
     if offer.start_date > Date.current
       errors.add(:base, I18n.t('public_files.error.offer_start')) if offer.start_date > Date.current
