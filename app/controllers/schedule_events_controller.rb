07602a7c7395aa909cbca84729d3068d25f792d2
Leeboy <pedro.ivo.freire.aragao@gmail.com>
2018-11-21 15:38:55 -0300

    [#157638052] Removendo os underscores no preenchimento do cabeçalho das provas preenchidas

--- app/controllers/schedule_events_controller.rb
+++ app/controllers/schedule_events_controller.rb
@@ -167,7 +167,7 @@ class ScheduleEventsController < ApplicationController
 
       student = User.find(params[:student_id]) unless params[:student_id].blank?
 
-      normalize_exam_header(html, student, profs, tutors, @event, allocation_tag.get_curriculum_unit, coord)
+      normalize_exam_header(html, student, profs, tutors, @event, allocation_tag.get_curriculum_unit, @course.name, coord)
 
       pictures_with_abs_path html
 
@@ -190,13 +190,14 @@ class ScheduleEventsController < ApplicationController
       html.sub!(pattern, name)
     end
 
-    def normalize_exam_header(html, student, profs, tutors, event, curriculum_unit, coord)
-      fill_field_info html, /disciplina:(\s*\n*\t*(&nbsp;)*)/i, "Disciplina: <b>#{curriculum_unit.code} - #{curriculum_unit.name}</b><br>" unless curriculum_unit.nil?
+    def normalize_exam_header(html, student, profs, tutors, event, curriculum_unit, course_name, coord)
+      fill_field_info html, /curso:(\s*\n*\t*(&nbsp;)*)_*/i, "Curso: <b>#{course_name}</b>" unless course_name.blank?
+      fill_field_info html, /disciplina:(\s*\n*\t*(&nbsp;)*)_*/i, "Disciplina: <b>#{curriculum_unit.code} - #{curriculum_unit.name}</b>" unless curriculum_unit.nil?
       # fill_field_info html, /(coordenador\(a\)(\s*\n*\t*(&nbsp;)*)do(\s*\n*\t*(&nbsp;)*)curso:|coordenador\(a\)(\s*\n*\t*(&nbsp;)*)da(\s*\n*\t*(&nbsp;)*)curso:|coordenador:(\s*\n*\t*(&nbsp;)*)coordenador\(a\)(\s*\n*\t*(&nbsp;)*)de(\s*\n*\t*(&nbsp;)*)curso:)/i, "Coordenador(a) do curso: #{coord.name}<br>" unless coord.nil?
-      fill_field_info html, /(nome(\s*\n*\t*(&nbsp;)*)do\(a\)(\s*\n*\t*(&nbsp;)*)aluno\(a\):(\s*\n*\t*(&nbsp;)*)|nome(\s*\n*\t*(&nbsp;)*)do(\s*\n*\t*(&nbsp;)*)aluno:|aluno:)/i, "Nome do(a) aluno(a): #{student.name}<br>" unless student.nil?
+      fill_field_info html, /(nome(\s*\n*\t*(&nbsp;)*)do\(a\)(\s*\n*\t*(&nbsp;)*)aluno\(a\):(\s*\n*\t*(&nbsp;)*)|nome(\s*\n*\t*(&nbsp;)*)do(\s*\n*\t*(&nbsp;)*)aluno:|aluno:)_*/i, "Nome do(a) aluno(a): <b>#{student.name}</b>" unless student.nil?
       unless event.blank?
-        fill_field_info html, /prova:(\s*\n*\t*(&nbsp;)*)/i, "Prova: <b>#{event.title}</b>"
-        fill_field_info html, /data:(\s*\n*\t*(&nbsp;)*)/i, "Data: #{event.get_date}"
+        fill_field_info html, /prova:(\s*\n*\t*(&nbsp;)*)_*/i, "Prova: <b>#{event.title}</b>"
+        fill_field_info html, /data:(\s*\n*\t*(&nbsp;)*)_*/i, "Data: <b>#{event.get_date}</b>"
       end
       profs.each { |prof| fill_field_info html, /(professor\(a\)(\s*\n*\t*(&nbsp;)*)titular:(\s*\n*\t*(&nbsp;)*)|professor(\s*\n*\t*(&nbsp;)*)titular:(\s*\n*\t*(&nbsp;)*)|professor:(\s*\n*\t*(&nbsp;)*)|coordenador\(a\)(\s*\n*\t*(&nbsp;)*)de(\s*\n*\t*(&nbsp;)*)disciplina:(\s*\n*\t*(&nbsp;)*)|coordenador\(a\)(\s*\n*\t*(&nbsp;)*)da(\s*\n*\t*(&nbsp;)*)disciplina:(\s*\n*\t*(&nbsp;)*))/i, "Professor(a) da disciplina: #{prof.name}<br>"  } unless profs.blank?
       tutors.each { |tutor| fill_field_info html, /(tutor\(a\)(\s*\n*\t*(&nbsp;)*)à(\s*\n*\t*(&nbsp;)*)distância:(\s*\n*\t*(&nbsp;)*)|tutor(\s*\n*\t*(&nbsp;)*)à(\s*\n*\t*(&nbsp;)*)distância:(\s*\n*\t*(&nbsp;)*)|tutor:(\s*\n*\t*(&nbsp;)*))/i, "Tutor(a) à distância: #{tutor.name}<br>"  } unless tutors.blank?
