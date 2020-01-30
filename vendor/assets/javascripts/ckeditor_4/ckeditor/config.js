09830e153efdccbe265f27e39b364a5fbe682266
Cristiano Lima <cristiano@virtual.ufc.br>
2020-01-30 14:07:23 -0300

    [#169153452] Correção no ckeditor dos posts

--- vendor/assets/javascripts/ckeditor_4/ckeditor/config.js
+++ vendor/assets/javascripts/ckeditor_4/ckeditor/config.js
@@ -1,38 +1,22 @@
-/**
- * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
- */
+CKEDITOR.on( 'dialogDefinition', function( ev ) {
+  var dialogName       = ev.data.name; // Recupera o nome da janela
+  var dialogDefinition = ev.data.definition; // Recupera dados da janela
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here.
-	// For complete reference see:
-	// https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
+  if ( dialogName == 'link' ) { // Se for a janela de "Link"
+    dialogDefinition.removeContents('advanced'); // Remove aba de configurações avançadas
 
-	// The toolbar groups arrangement, optimized for two toolbar rows.
-	config.toolbarGroups = [
-	    { name: 'clipboard',   groups: [ 'cut', 'copy', 'paste', 'pasteText', 'pasteFromWord', '-', 'undo', 'redo' ] },
-	    { name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-	    { name: 'links',  groups: ['link', 'unlink', 'image', 'oembed', 'smiley' ] },
-	    { name: 'insert', groups: ['eqneditor' ] },
-	    { name: 'forms' },
-	    { name: 'tools' },
-	    { name: 'document',    groups: [ 'mode', 'document', 'doctools' ] },
-	    { name: 'others' },
-	    '/',
-	    { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup', 'bold', 'italic', 'underline', 'strike', '-', 'removeformat' ] },
-	    { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-	    { name: 'styles',   groups: [ 'styles', 'format', 'font', 'textcolor' ] },
-	    { name: 'colors' },
-	    { name: 'about' }
-	  ];
+    var target = ev.data.definition.getContents('target');
+    var options = target.get('linkTargetType').items;
+    for (var i = options.length-1; i >= 0; i--) {
+        var label = options[i][0];
+        if (!label.match(/_blank/i)) {
+            options.splice(i, 1);
+        }
+    }
+    var targetField = target.get( 'linkTargetType' );
+    targetField['default'] = '_blank';
 
-	// Remove some buttons provided by the standard plugins, which are
-	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
-
-	// Set the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;pre';
-
-	// Simplify the dialog windows.
-	config.removeDialogTabs = 'image:advanced;link:advanced';
-};
+    //    var infoTab = dialogDefinition.getContents( 'target'); // Recupera as informações da aba de destino do link
+    //    infoTab.elements[0].children[0].default = '_blank'; // Seta como o padrão sendo "_blank"
+  }
+});
