a818e5b659e4fba0982f0868376605abd8ab3de9
henriquebol <henbolima@gmail.com>
2020-08-06 13:12:02 -0300

    Removendo force_ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,6 @@
 Solar::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
-
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true
