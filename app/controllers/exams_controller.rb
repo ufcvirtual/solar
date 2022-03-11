8e43938b232a66ff8f79bb65905d907ff38610e9
Cristiano Lima <cristiano@virtual.ufc.br>
2022-03-11 09:34:51 -0300

    Sessão no redis

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -16,8 +16,8 @@ class ExamsController < ApplicationController
     @can_open = can? :open, Exam, {on: @allocation_tag_id}
     @can_evaluate = can? :evaluate, Exam, { on: @allocation_tag_id }
     @is_student = current_user.is_student?([@allocation_tag_id])
-  rescue => error
-    render json: { success: false, alert: t(:no_permission) }, status: :unauthorized
+  #rescue => error
+    #render json: { success: false, alert: t(:no_permission) }, status: :unauthorized
   end
 
   def new
@@ -115,18 +115,26 @@ class ExamsController < ApplicationController
       last_attempt.update_attribute(:complete, true)
       last_attempt = @acu.exam_user_attempts.last
     end
+
+    total_duration = last_attempt.blank? ? 0 : last_attempt.get_total_duration
+
     raise 'time' unless @exam.on_going?
-    raise 'attempt' unless @acu.has_attempt(@exam)
+    raise 'attempt' unless @acu.has_attempt(@exam, last_attempt)
     @total_attempts  = @acu.count_attempts rescue 0
     @total_time = (last_attempt.try(:complete) ? 0 : last_attempt.try(:get_total_time)) || 0
     duration = (@total_time/60).to_i
-    if @total_time==0 && last_attempt.blank? && !@exam.end_hour.blank?
+    p @exam.duration
+    p (@total_time/60).to_i
+    if @total_time==0 && (last_attempt.blank? || total_duration==0) && !@exam.end_hour.blank?
+      p 'TESTE 004'
       exam_end = @exam.schedule.end_date.to_s+' '+@exam.end_hour.to_s
       difference_minutes = (Time.parse(exam_end) - current_time_db) / 60
       @total_time = (difference_minutes.to_i > @exam.duration.to_i) ? @exam.duration : (@exam.duration.to_i-difference_minutes)
       @total_time = @total_time*60
     end
-    if(last_attempt.try(:uninterrupted_or_ended, @exam)) && @total_attempts == @exam.attempts
+    
+    @total_attempts = @exam.attempts<@total_attempts ? @total_attempts+1 : @exam.attempts
+    if (last_attempt.try(:uninterrupted_or_ended, @exam) && @total_attempts == @exam.attempts && total_duration>0)
       redirect_to result_user_exam_path(@exam)
     else
       @text = if !last_attempt.blank? && !last_attempt.try(:complete) && !@exam.uninterrupted
@@ -145,7 +153,7 @@ class ExamsController < ApplicationController
     @last_attempt.get_total_time(nil, true)
 
     @total_time = (@last_attempt.try(:complete) ? 0 : @last_attempt.try(:get_total_time)) || 0
-
+    total_duration = @last_attempt.get_total_duration
     if(@exam.ended? && (@situation == 'finished' || @situation == 'corrected' || @situation == 'evaluated'))
       mod_correct_exam = @exam.attempts_correction
       @exam_user_attempt = ExamUserAttempt.where(id: params[:exam_user_attempt_id]).first
@@ -192,10 +200,10 @@ class ExamsController < ApplicationController
         end
       end
     end
-  rescue CanCan::AccessDenied
-    render plain: t(:no_permission)
-  rescue => error
-    render plain: error.to_s
+  #rescue CanCan::AccessDenied
+  #  render plain: t(:no_permission)
+  #rescue => error
+  #  render plain: error.to_s
   end
 
   def result_exam_user
