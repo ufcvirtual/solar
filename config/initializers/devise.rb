9b75088dcb58c78a10678cfec5e8cee2f0b89618
Danniel Albuquerque <danniel.master@gmail.com>
2022-08-17 14:18:16 -0300

    ajustes docker-compose

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,12 +6,12 @@ Devise.setup do |config|
 # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
-  config.secret_key = "#{ENV["DEVISE_SECRET_KEY"]}"
+  config.secret_key = ENV["DEVISE_SECRET_KEY"]
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "#{ENV["DEFAULT_SENDER"]}"
+  config.mailer_sender = ENV["DEFAULT_SENDER"]
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
