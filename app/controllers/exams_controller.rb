2b864b9cc6911dbbb68fa8ac7e90318b86abf0d4
Bianca <bianca.stephani@gmail.com>
2023-05-19 12:35:27 -0300

    Correção de prova com mais tentativas

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -111,14 +111,14 @@ class ExamsController < ApplicationController
 
     last_attempt.get_total_time(nil, true) if !last_attempt.blank? && !last_attempt.end.blank?
     total_time = last_attempt.blank? ? 0 : (last_attempt.try(:get_total_time)/60).to_i
-    if total_time>=@exam.duration.to_i || (!last_attempt.blank? && @exam.uninterrupted) 
+    if total_time>=@exam.duration.to_i || (!last_attempt.blank? && @exam.uninterrupted)
       last_attempt.update_attribute(:complete, true)
       last_attempt = @acu.exam_user_attempts.last
     end
 
     #if @exam.uninterrupted
     #  last_attempt = nil
-    #end  
+    #end
     total_duration = last_attempt.blank? ? 0 : last_attempt.get_total_duration
 
     raise 'time' unless @exam.on_going?
@@ -133,7 +133,7 @@ class ExamsController < ApplicationController
       @total_time = @total_time*60
     end
 
-    @total_attempts = @exam.attempts>@total_attempts ? @total_attempts+1 : @exam.attempts
+    @total_attempts = @exam.attempts>@total_attempts ? @total_attempts : @exam.attempts
     if (last_attempt.try(:uninterrupted_or_ended, @exam) && @total_attempts == @exam.attempts && total_duration>0)
       redirect_to result_user_exam_path(@exam)
     else
